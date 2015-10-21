9027845d72007b4bb07311db50d1a0c249b765f8
Justin Ales <justin.ales@gmail.com>
2015-10-21 21:51:06 +0100

    Reverted the exampleExperiment paradigm file, and moved changed to the rotating gabor paradigm

--- psychMaster/psychParadigm_exampleExperiment.m
+++ psychMaster/psychParadigm_exampleExperiment.m
@@ -1,26 +1,28 @@
-function [conditionInfo, ScreenInfo, screenInfo] = psychparadigm_rotating_gabor(screenInfo)
+function [conditionInfo, screenInfo] = psychParadigm_exampleExperiment(screenInfo)
+
+%screenInfo contains experiment wide settings. C
+%conditionInfo contains per condition parameters.
+
+%paradigmName is what will be prepended to data files
 screenInfo.paradigmName = 'noiseDetect';
 
-% use kbQueue's as they have high performance
-screenInfo.useKbQueue = false:
-
+%Let's use kbQueue's because they have high performance.
+screenInfo.useKbQueue = true;
 
 %Lets add an experiment wide setting here:
 payoff = [ 10 10 -10 -10];
 screenInfo.payoff = payoff;
 
 screenInfo.instructions = ['Press f key if target present' ...
-         '\nPress f key if target absent\n' ...
+         '\nPress j key if target absent\n' ...
          '\n' num2str(payoff(1)) ' points for a hit' ...
          '\n' num2str(payoff(2)) ' points for a correct reject' ...
          '\n' num2str(payoff(3)) ' points for a miss' ...
          '\n ' num2str(payoff(4)) ' points for a false alarm' ...
          '\n\n    Press any key to start'];
-%%% Justin**** b unsure about wether to leave all these screen instructions in??
      
 %This defines what function to call to draw the condition
-conditionInfo(1).trialFun=rotating_gabor_trial;
-
+conditionInfo(1).trialFun=@exampleNoiseTrial;
 
 % %Condition definitions
 %Condition 1, lets set some defaults:
@@ -30,15 +32,11 @@ conditionInfo(1).preStimDuration  = 0.5;  %Static time before stimulus change
 conditionInfo(1).postStimDuration = 0;  %static time aftter stimulus change
 conditionInfo(1).iti              = .2;     %Inter Stimulus Interval
 conditionInfo(1).responseDuration = 2;    %Post trial window for waiting for a response
-conditionInfo(1).sigma=.20; %cyclespersigma
-conditionInfo(1).freq = 4; %frequency of what ***justin
-conditionInfo(1).targetAmp = 0; % targetamplitude **Justin
-conditionInfo(1).nReps = 2; %% number of reps **justin
-conditionInfo(1).stimRadiusCm   = 5;    %stimulus size in cm;
-conditionInfo(1).contrast = 0.25 
-conditonInfo(1).orientation = 360*Rand() 
-
-
+conditionInfo(1).sigma=.2;
+conditionInfo(1).freq = 4;
+conditionInfo(1).targetAmp = 0;
+conditionInfo(1).nReps = 2;
+conditionInfo(1).stimRadiusCm   = 1;    %stimulus size in cm;
 
 %For conditions 2-4 we're going to copy all the settings from condition 1
 %and just define what we want changed.
