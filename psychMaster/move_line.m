a29513fcc661052028bd096a1de6f7170b4e506f
abigailrilee <Abigail@psmac-08343.st-andrews.ac.uk>
2015-10-27 12:16:18 +0000

    The line moves until the 1000th pixel in the x axis

--- psychMaster/move_line.m
+++ psychMaster/move_line.m
@@ -39,33 +39,46 @@ x=0; %xcoordinate
 xv = 10;
 lw = 1; %linewidth
 % 
-% while ();
+% while (KbStrokeWait); %this moves the line while you hold down a key on 
+%     %the keyboard. Want it to do the opposite and run until you press a key
+%     %but it's not as simple as just doing ~KbStrokeWait.
 %     
 % x=mod(x+xv, screenXpixels);
+% Screen('DrawLines', window, [x, x ; 0, screenYpixels], lw); 
+% vbl=Screen('Flip', window,vbl+ifi/2); %taken from PTB-3 MovingLineDemo
 % 
 % end
 
-button = 0;
+% button = 0;
+% 
+% %Run until left mouse button is pressed:
+% while ~button(1)
+%     %Query mouse:
+%     [xm, ym, button] = GetMouse;
+%     
+%     %Move line pair by 'xv' unless right mouse button is pressed, which
+%     %will pause the animation:
+%     if button(2)==0
+%         x=mod(x+xv, screenXpixels);
+%         Screen('DrawLines', window, [x, x ; 0, screenYpixels], lw); 
+%         vbl=Screen('Flip', window,vbl+ifi/2); %taken from PTB-3 MovingLineDemo
+% 
+%     end 
+% end
 
-%Run until left mouse button is pressed:
-while ~button(1)
-    %Query mouse:
-    [xm, ym, button] = GetMouse;
-    
-    %Move line pair by 'xv' unless right mouse button is pressed, which
-    %will pause the animation:
-    if button(2)==0
-        x=mod(x+xv, screenXpixels);
+while x<1000; %so now you need to change this so that the x ==/less than/ 
+    %whatever value is entered at the beginning. you also need to pick the
+    %x coordinate to start at at the beginning.
+     x=mod(x+xv, screenXpixels);
         Screen('DrawLines', window, [x, x ; 0, screenYpixels], lw); 
         vbl=Screen('Flip', window,vbl+ifi/2); %taken from PTB-3 MovingLineDemo
 
     end 
-end
 %this currently moves the position of the line across the screen until the
 %left mouse button is pressed. right click pauses (so if right click/button
 %(2) is false then it will run.
 
-%KbStrokeWait;
+KbStrokeWait;
 
 %This would be the end of your trial script. 
 %%%%%
