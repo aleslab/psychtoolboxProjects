99c62219706ba384ccb4b3a7c3dae8efcdccc4c4
abigailrilee <abigailrilee@googlemail.com>
2015-11-12 16:46:35 +0000

    A few more simple tests
    A few more simple tests where the fixation point is directly in front of
    the eye.

--- psychMaster/testCalculateScreenLocation.m
+++ psychMaster/testCalculateScreenLocation.m
@@ -17,7 +17,7 @@ expectedScreenY = 0;
 assert(screenX==expectedScreenX,'Failure');
 assert(screenY==expectedScreenY,'Failure');
 
-
+%2
 %Input
 viewingDistance = 57;
 objectX = 10;
@@ -32,7 +32,7 @@ expectedScreenY = 0;
 assert(screenX==expectedScreenX,'Failure');
 assert(screenY==expectedScreenY,'Failure');
 
-
+%3
 %Input
 viewingDistance = 57;
 objectX = 10;
@@ -48,5 +48,52 @@ assert(abs(screenX-expectedScreenX)<.001,'Failure');
 assert(abs(screenY-expectedScreenY)<.001,'Failure');
 
 
+%4
+
+%Input
+viewingDistance = 57;
+objectX = 25;
+objectY = 0;
+objectZ = 25;
+
+%expected result
+expectedScreenX = 44.53125;
+expectedScreenY = 0;
+[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+
+assert(abs(screenX-expectedScreenX)<.01,'Failure');
+assert(abs(screenY-expectedScreenY)<.01,'Failure');
+
+%5
+
+%Input
+viewingDistance = 57;
+objectX = -10;
+objectY = 0;
+objectZ = 10;
+
+%expected result
+expectedScreenX = -12.12765957;
+expectedScreenY = 0;
+[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+
+assert(abs(screenX-expectedScreenX)<.001,'Failure');
+assert(abs(screenY-expectedScreenY)<.001,'Failure');
+
 disp('It worked');
 
+%6
+
+%Input
+viewingDistance = 57;
+objectX = 10;
+objectY = 0;
+objectZ = -10;
+
+%expected result
+expectedScreenX = 8.507;
+expectedScreenY = 0;
+[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+
+assert(abs(screenX-expectedScreenX)<.001,'Failure');
+assert(abs(screenY-expectedScreenY)<.001,'Failure');
