2e710414f8a4ad75e0286e81916716b306869f12
abigailrilee <abigailrilee@googlemail.com>
2015-11-18 14:11:36 +0000

    An attempt at finding screenX when there isn't a right angle triangle between the eye and fixation point.
    I'm not sure if the logic of what I've done is sound in the
    calculateScreenLocation.m file, but if I test it with specific values it
    does produce the result I'm expecting. However, it doesn't work if A is
    closer to the fixation point in the X axis than the eye is -- But I'm
    not sure if this is a problem or not. Quite a lot to think about.

--- psychMaster/calculateScreenLocation.m
+++ psychMaster/calculateScreenLocation.m
@@ -1,11 +1,41 @@
-function [screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance)
+function [screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance, IOD)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
+%% for when the eye and fixation point give a right-angle triangle.
 
-thetaPrime = atan ( objectX / (viewingDistance - objectZ));
-screenX = viewingDistance*tan(thetaPrime);
-screenY = objectY;
+% thetaPrime = atan ( objectX / (viewingDistance - objectZ));
+% screenX = viewingDistance*tan(thetaPrime);
+% screenY = objectY;
+
+%% for when the eye and fixation point do not form a right-angle triangle
+if objectX >= 0;
+    halfId = IOD/2;
+else
+    halfId = (-IOD)/2;
+end
+%Half of the interocular distance -- the distance the eye
+%should be from the fixation point. If we're moving to the left of the
+%fixation point, the half IOD will be a negative value as the fixation
+%point is 0 in x.
+BC = sqrt((halfId^2)+(viewingDistance^2)); %distance between the fixation 
+theta = 2*atan(objectX/(2*viewingDistance)); %initial visual angle
+%point and the centre of the eye
+angleX = asin(halfId/BC); %rearranged sine rule
+angleZ = (pi/2) - angleX; % angles in a triangle
+angleY = pi - theta - angleZ; %angles in a triangle
+angleTau = (pi/2) - angleY; % tau = pi - pi/2 - angleY will simplify to this
+AC = (objectX*sin(angleZ))/sin(theta); %the distance between the centre of 
+%the eye and the objectX (A) starting point
+lengthOmega = sqrt((objectZ^2)+(AC^2)- (2*objectZ*AC*cos(angleTau))); %cosine rule
+angleBeta = asin((viewingDistance - objectZ)/lengthOmega); %rearranged sine rule
+angleDelta = (pi/2) - angleBeta; %pi - pi/2 - angleBeta simplifies to this; angles in a triangle add up to pi rads
+lengthAPrimeA = (objectZ*sin(angleDelta))/(sin(angleBeta)); %using sine rule to find A'A
+screenX = objectX + lengthAPrimeA; %A'B = A'A + AB
+screenY = objectY; % 0 for both
+
+
+
 
 end
 
--- psychMaster/testCalculateScreenLocation.m
+++ psychMaster/testCalculateScreenLocation.m
@@ -1,99 +1,166 @@
 %Tests for calculating screen location for object moving in depth
 
+% %1
+% %[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance)
+% 
+% %Input
+% viewingDistance = 57;
+% objectX = 0;
+% objectY = 0;
+% objectZ = 0;
+% 
+% %expected result
+% expectedScreenX = 0;
+% expectedScreenY = 0;
+% [screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+% 
+% assert(screenX==expectedScreenX,'Failure');
+% assert(screenY==expectedScreenY,'Failure');
+% 
+% %2
+% %Input
+% viewingDistance = 57;
+% objectX = 10;
+% objectY = 0;
+% objectZ = 0;
+% 
+% %expected result
+% expectedScreenX = 10;
+% expectedScreenY = 0;
+% [screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+% 
+% assert(screenX==expectedScreenX,'Failure');
+% assert(screenY==expectedScreenY,'Failure');
+% 
+% %3
+% %Input
+% viewingDistance = 57;
+% objectX = 10;
+% objectY = 0;
+% objectZ = 10;
+% 
+% %expected result
+% expectedScreenX = 12.12765957;
+% expectedScreenY = 0;
+% [screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+% 
+% assert(abs(screenX-expectedScreenX)<.001,'Failure');
+% assert(abs(screenY-expectedScreenY)<.001,'Failure');
+% 
+% 
+% %4
+% 
+% %Input
+% viewingDistance = 57;
+% objectX = 25;
+% objectY = 0;
+% objectZ = 25;
+% 
+% %expected result
+% expectedScreenX = 44.53125;
+% expectedScreenY = 0;
+% [screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+% 
+% assert(abs(screenX-expectedScreenX)<.01,'Failure');
+% assert(abs(screenY-expectedScreenY)<.01,'Failure');
+% 
+% %5
+% 
+% %Input
+% viewingDistance = 57;
+% objectX = -10;
+% objectY = 0;
+% objectZ = 10;
+% 
+% %expected result
+% expectedScreenX = -12.12765957;
+% expectedScreenY = 0;
+% [screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+% 
+% assert(abs(screenX-expectedScreenX)<.001,'Failure');
+% assert(abs(screenY-expectedScreenY)<.001,'Failure');
+% 
+% disp('It worked');
+% 
+% %6
+% 
+% %Input
+% viewingDistance = 57;
+% objectX = 10;
+% objectY = 0;
+% objectZ = -10;
+% 
+% %expected result
+% expectedScreenX = 8.507;
+% expectedScreenY = 0;
+% [screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+% 
+% assert(abs(screenX-expectedScreenX)<.001,'Failure');
+% assert(abs(screenY-expectedScreenY)<.001,'Failure');
+
+%% for when the fixation point and centre of the eye do not produce a right angle
+
 %1
-%[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance)
-
-%Input
-viewingDistance = 57;
-objectX = 0;
-objectY = 0;
-objectZ = 0;
-
-%expected result
-expectedScreenX = 0;
-expectedScreenY = 0;
-[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
-
-assert(screenX==expectedScreenX,'Failure');
-assert(screenY==expectedScreenY,'Failure');
-
-%2
-%Input
-viewingDistance = 57;
-objectX = 10;
-objectY = 0;
-objectZ = 0;
+%input
+viewingDistance = 57; %Vd
+objectX = 10; %AB
+objectY = 0; %ignore
+objectZ = 10; %d
+IOD = 6.3; %Interocular distance, Id
 
 %expected result
-expectedScreenX = 10;
+expectedScreenX = 11.36807813;
 expectedScreenY = 0;
-[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
-
-assert(screenX==expectedScreenX,'Failure');
-assert(screenY==expectedScreenY,'Failure');
-
-%3
-%Input
-viewingDistance = 57;
-objectX = 10;
-objectY = 0;
-objectZ = 10;
-
-%expected result
-expectedScreenX = 12.12765957;
-expectedScreenY = 0;
-[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
-
-assert(abs(screenX-expectedScreenX)<.001,'Failure');
-assert(abs(screenY-expectedScreenY)<.001,'Failure');
-
-
-%4
-
-%Input
-viewingDistance = 57;
-objectX = 25;
-objectY = 0;
-objectZ = 25;
-
-%expected result
-expectedScreenX = 44.53125;
-expectedScreenY = 0;
-[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance, IOD); 
+%altered this so that it feeds into a function that does this when there
+%isn't a right angle.
 
 assert(abs(screenX-expectedScreenX)<.01,'Failure');
 assert(abs(screenY-expectedScreenY)<.01,'Failure');
-
-%5
-
-%Input
-viewingDistance = 57;
-objectX = -10;
-objectY = 0;
-objectZ = 10;
-
-%expected result
-expectedScreenX = -12.12765957;
-expectedScreenY = 0;
-[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
-
-assert(abs(screenX-expectedScreenX)<.001,'Failure');
-assert(abs(screenY-expectedScreenY)<.001,'Failure');
-
 disp('It worked');
 
-%6
+%2
+% this doesn't work... If the interocular distance were 0, you would expect
+% the value to be the same as when we do the right angle triangle
+% calculations... wouldn't you? But it isn't, it comes out as 11.6338 instead
+% of 12.1277.
 
-%Input
-viewingDistance = 57;
-objectX = 10;
-objectY = 0;
-objectZ = -10;
+% viewingDistance = 57; %Vd
+% objectX = 10; %AB
+% objectY = 0; %ignore
+% objectZ = 10; %d
+% IOD = 0; %Interocular distance, Id
+% 
+% %expected result
+% expectedScreenX = 12.12765957;
+% expectedScreenY = 0;
+% [screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance, IOD); 
+% %altered this so that it feeds into a function that does this when there
+% %isn't a right angle.
+% 
+% assert(abs(screenX-expectedScreenX)<.01,'Failure');
+% assert(abs(screenY-expectedScreenY)<.01,'Failure');
+% disp('It worked');
 
-%expected result
-expectedScreenX = 8.507;
-expectedScreenY = 0;
-[screenX,screenY] = calculateScreenLocation(objectX,objectY,objectZ,viewingDistance);
+%3 
+%When A is between B and C in the X axis
 
-assert(abs(screenX-expectedScreenX)<.001,'Failure');
-assert(abs(screenY-expectedScreenY)<.001,'Failure');
+% viewingDistance = 57; %Vd
+% objectX = -2; %AB
+% objectY = 0; %ignore
+% objectZ = 10; %d
+% IOD = 6.3; %Interocular distance, Id
+
+%gets to the stage of calculating angleBeta but then asin(47/46.93264125)
+%(asin(vd-d/angleOmega)) is bigger than 1, which is impossible, so a math error
+%is returned. So this method of working screenX (A'B) out doesn't work for 
+%this scenario.
+
+%But would you ever want this scenario? Because in theory, to move towards
+%the observer when the starting point is between the point at the centre 
+%of the eye and the fixation point, wouldn't the line generated on the
+%screen need to fairly spontaneously change direction -- which might cause
+%problems? 
+
+%Would we therefore want to make the code so that there is always at least
+%the interocular distance between the lines on the screen?
