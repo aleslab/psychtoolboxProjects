fdbd50dbc0a2c7cb55936962127abcff84831b7f
abigailrilee <Abigail@psmac-08343.st-andrews.ac.uk>
2016-02-02 10:34:19 +0000

    Small edits to instructions
    Small edits to the experiment instructions and pause information so
    that it would fit on the split screen properly without being cut off.

--- psychMaster/moveLineDefaultSettings.m
+++ psychMaster/moveLineDefaultSettings.m
@@ -3,5 +3,5 @@ function [expInfo] = moveLineDefaultSettings(expInfo)
 expInfo.stereoMode = 4; %0 is monocular, 4 is split screen, 8 is anaglyph
 expInfo.giveFeedback = false;
 %expInfo.useFullScreen = true;
-expInfo.instructions = 'Which one moved faster?\nPress any key to begin';
-expInfo.pauseInfo = 'Paused\nPress any key to continue';
+expInfo.instructions = 'Which one moved\nfaster?\nPress any key\nto begin';
+expInfo.pauseInfo = 'Paused\nPress any key to\ncontinue';
