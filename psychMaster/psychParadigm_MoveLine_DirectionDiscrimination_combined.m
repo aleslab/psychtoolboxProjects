4d7214805553b2e9210fc48b53174643ca767cc0
abigailrilee <abigailrilee@googlemail.com>
2016-04-06 13:40:21 +0100

    Edits to paradigm files
    not running correctly the way they were

--- psychMaster/psychParadigm_MoveLine_DirectionDiscrimination_combined.m
+++ psychMaster/psychParadigm_MoveLine_DirectionDiscrimination_combined.m
@@ -6,27 +6,27 @@ function [conditionInfo, expInfo] = psychParadigm_MoveLine_DirectionDiscriminati
 %for the combined (double vertical line) version of this.
 
 expInfo = moveLineDefaultSettings(expInfo);
-
+expInfo.instructions = 'What direction did the stimulus move in?';
 expInfo.paradigmName = 'MoveLine_DirectionDiscrimination_combined';
 %% conditions
-firstVelocities = repmat([-20, 20],4); % first section velocities are all 20cms-1 but in different directions
+firstVelocities = cat(2,repmat([-20, 20],1,4)); % first section velocities are all 20cms-1 but in different directions
 condStimTypes = cat(2, repmat( {'combined'},1,4) , repmat( {'lateralCombined'},1,4)); %condition types cd cd lateralCd lateralCd
-stimDuration1 = repmat([0.25 0.25 0.5 0.5],2);
+stimDuration1 = cat(2,repmat([0.25 0.25 0.5 0.5],1,2));
 stimDuration2 = stimDuration1;
-depthStart = repmat([5 -5 10 -10],2);
+depthStart = cat(2,repmat([5 -5 10 -10], 1,2));
+
 for iCond = 1: length(firstVelocities);
     
 %This defines what function to call to draw the condition
 conditionInfo(iCond).trialFun=@MoveLineTrial;
 
-conditionInfo(iCond).type             = 'dirDiscrimination'; 
+conditionInfo(iCond).type             = 'directionreport'; 
 conditionInfo(iCond).stimType         = condStimTypes{iCond}; 
 conditionInfo(iCond).stimDurationSection1 = stimDuration1(iCond); %approximate stimulus duration in seconds
 conditionInfo(iCond).stimDurationSection2 = stimDuration2(iCond);
 conditionInfo(iCond).preStimDuration  = 0.25;  %Static time before stimulus change
 conditionInfo(iCond).postStimDuration = 0;  %static time after stimulus change 
 %may need to reintroduce something for post stimduration because it can get confusing when doing the experiment
-conditionInfo(iCond).iti              = 1;     %Inter Stimulus Interval
 conditionInfo(iCond).responseDuration = 3;    %Post trial window for waiting for a response
 conditionInfo(iCond).velocityCmPerSecSection1 = firstVelocities(iCond); %cm/s
 conditionInfo(iCond).velocityCmPerSecSection2 = firstVelocities(iCond); %cm/s 
