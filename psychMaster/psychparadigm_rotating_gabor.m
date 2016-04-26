ba4d336ad5a29057268b179fe00447c7147b7cca
fraser <fa28@st-andrews.ac.uk>
2016-04-26 10:10:48 +0100

    Updated rotating gabor options

--- psychMaster/psychparadigm_rotating_gabor.m
+++ psychMaster/psychparadigm_rotating_gabor.m
@@ -4,7 +4,7 @@ expInfo.paradigmName = 'randomtrack';
 % use kbQueue's as they have high performance
 expInfo.useKbQueue = false;
 expInfo.enablePowermate = true;
-
+expInfo.viewingDistance = 57;
 if expInfo.enablePowermate
 dev = PsychHID('devices');
 
@@ -30,19 +30,16 @@ conditionInfo(1).giveFeedback = true;
 
 % %Condition definitions
 %Condition 1, lets set some defaults:
-conditionInfo(1).stimDuration     = 50; %approximate stimulus duration in seconds
-conditionInfo(1).stimDuration     = 50; %approximate stimulus duration in seconds
+conditionInfo(1).stimDuration     = 10; %approximate stimulus duration in seconds
 conditionInfo(1).preStimDuration  = 2;  %Static time before stimulus change
-% =======
-% conditionInfo(1).preStimDuration  = 0.5;  %Static time before stimulus change
-% >>>>>>> Stashed changes
+
 conditionInfo(1).postStimDuration = 0;  %static time aftter stimulus change
 conditionInfo(1).iti              = 1;     %Minimum Inter Trial Interval
 conditionInfo(1).responseDuration  = 0;    %Post trial window for waiting for a response
 
 conditionInfo(1).sigma             =2; %standard deviation of the gabor in degrees
 conditionInfo(1).freq              =1; %frequency of the gabor in cycles per sigma. 
-conditionInfo(1).nReps             = 3; %% number of trials to present this condition. 
+conditionInfo(1).nReps             = 5; %% number of trials to present this condition. 
 conditionInfo(1).stimRadiusDeg  = 6;    %stimulus size in degree;
 
 conditionInfo(1).contrast = 0.25;
@@ -50,16 +47,18 @@ conditionInfo(1).noiseSigma = .15;
 conditionInfo(1).orientationSigma = 5;
 %Implement arbitrary forward models. 
 %conditionInfo(1).forwardModel = [ 1 0 ]; %Forward model
+conditionInfo(1).label = 'Contrast: 0.25'
 
 
 conditionInfo(2) = conditionInfo(1);
 conditionInfo(2).orientationSigma = 5;
 conditionInfo(2).contrast = 0.04 ;
+conditionInfo(2).label = 'Contrast: 0.04'
 
 conditionInfo(3) = conditionInfo(1);
 conditionInfo(3).orientationSigma = 5;
 conditionInfo(3).contrast = 0.08;
-
+conditionInfo(3).label = 'Contrast: 0.08'
 
 
 
