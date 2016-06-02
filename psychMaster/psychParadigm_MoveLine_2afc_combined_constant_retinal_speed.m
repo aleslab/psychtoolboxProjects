3f9cfd286244d64573567cfb5946125a5189ec53
abigailrilee <abigailrilee@googlemail.com>
2016-06-02 17:18:24 +0100

    Edits to constant retinal speed paradigm file

--- psychMaster/psychParadigm_MoveLine_2afc_combined_constant_retinal_speed.m
+++ psychMaster/psychParadigm_MoveLine_2afc_combined_constant_retinal_speed.m
@@ -7,8 +7,7 @@ expInfo = moveLineDefaultSettings(expInfo);
 expInfo.paradigmName = 'MoveLine_combined_constant_retinal_speed';
 
 %% conditions
-%firstVelocities = [-20:2.5:-5 -20:2.5:-5];
-firstVelocities = [-20:-2.5:-35];
+firstVelocities = 8:1:14;
 condStimTypes = repmat({'combined_retinal'},1,7);
 
 for iCond = 1: length(firstVelocities);
@@ -26,18 +25,18 @@ conditionInfo(iCond).postStimDuration = 0;  %static time after stimulus change
 conditionInfo(iCond).iti              = 1;     %Inter Stimulus Interval
 conditionInfo(iCond).responseDuration = 3;    %Post trial window for waiting for a response
 conditionInfo(iCond).velocityCmPerSecSection1 = firstVelocities(iCond); %cm/s
-conditionInfo(iCond).velocityCmPerSecSection2 = (-40)-(conditionInfo(iCond).velocityCmPerSecSection1); %cm/s 
+conditionInfo(iCond).velocityCmPerSecSection2 = (16)-(conditionInfo(iCond).velocityCmPerSecSection1); %cm/s 
 conditionInfo(iCond).isNullCorrect = false;
-conditionInfo(iCond).objectOneStartPos = -1; %when there are two lines in each eye, the start position of the first line
-conditionInfo(iCond).objectTwoStartPos = 1; %the start position of the second line in each eye
+conditionInfo(iCond).objectOneStartPos = -5; %when there are two lines in each eye, the start position of the first line
+conditionInfo(iCond).objectTwoStartPos = -3; %the start position of the second line in each eye
 conditionInfo(iCond).nReps = 30; %number of repeats
 conditionInfo(iCond).giveFeedback = false;
 conditionInfo(iCond).depthStart = 0; %5cm behind the plane of the screen
 conditionInfo(iCond).label = [ condStimTypes{iCond} '_' num2str(firstVelocities(iCond))];
 
 nullCondition = conditionInfo(iCond);
-nullCondition.velocityCmPerSecSection1 = -20;  
-nullCondition.velocityCmPerSecSection2 = -20;
+nullCondition.velocityCmPerSecSection1 = 8;  
+nullCondition.velocityCmPerSecSection2 = 8;
 nullCondition.stimType = condStimTypes(iCond); 
 conditionInfo(iCond).nullCondition = nullCondition;
 
