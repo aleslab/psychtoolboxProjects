cc9477734e883ec208672bfdcb81de73aba84031
abigailrilee <abigailrilee@googlemail.com>
2016-06-15 13:19:03 +0100

    constant retinal speed task with slow speeds
    paradigm file for a constant retinal speed task with speeds close to
    what we use in the depth experiment.

--- psychMaster/psychParadigm_MoveLine_2afc_combined_slow_retinal_speed.m
+++ psychMaster/psychParadigm_MoveLine_2afc_combined_slow_retinal_speed.m
@@ -1,13 +1,13 @@
-function [conditionInfo, expInfo] = psychParadigm_MoveLine_2afc_combined_constant_retinal_speed(expInfo)
+function [conditionInfo, expInfo] = psychParadigm_MoveLine_2afc_combined_slow_retinal_speed(expInfo)
 
 %Paradigm file for the combined looming and cd stimulus. Two vertical lines
 %moving in each eye.
 expInfo = moveLineDefaultSettings(expInfo);
 %paradigmName is what will be prepended to data files
-expInfo.paradigmName = 'MoveLine_combined_constant_retinal_speed';
+expInfo.paradigmName = 'MoveLine_combined_constant_retinal_speed_slow';
 
 %% conditions
-firstVelocities = 8:1:14;
+firstVelocities = [0.629 0.643 0.725 0.810 0.894 0.984 1.072];
 condStimTypes = repmat({'combined_retinal'},1,7);
 
 for iCond = 1: length(firstVelocities);
@@ -25,18 +25,18 @@ conditionInfo(iCond).postStimDuration = 0;  %static time after stimulus change
 conditionInfo(iCond).iti              = 1;     %Inter Stimulus Interval
 conditionInfo(iCond).responseDuration = 3;    %Post trial window for waiting for a response
 conditionInfo(iCond).velocityCmPerSecSection1 = firstVelocities(iCond); %cm/s
-conditionInfo(iCond).velocityCmPerSecSection2 = (16)-(conditionInfo(iCond).velocityCmPerSecSection1); %cm/s 
+conditionInfo(iCond).velocityCmPerSecSection2 = (1.258)-(conditionInfo(iCond).velocityCmPerSecSection1); %cm/s 
 conditionInfo(iCond).isNullCorrect = false;
-conditionInfo(iCond).objectOneStartPos = -5; %when there are two lines in each eye, the start position of the first line
-conditionInfo(iCond).objectTwoStartPos = -3; %the start position of the second line in each eye
+conditionInfo(iCond).objectOneStartPos = -1.3145; %when there are two lines in each eye, the start position of the first line
+conditionInfo(iCond).objectTwoStartPos = 0.6855; %the start position of the second line in each eye
 conditionInfo(iCond).nReps = 30; %number of repeats
 conditionInfo(iCond).giveFeedback = false;
 conditionInfo(iCond).depthStart = 0; %5cm behind the plane of the screen
 conditionInfo(iCond).label = [ condStimTypes{iCond} '_' num2str(firstVelocities(iCond))];
 
 nullCondition = conditionInfo(iCond);
-nullCondition.velocityCmPerSecSection1 = 8;  
-nullCondition.velocityCmPerSecSection2 = 8;
+nullCondition.velocityCmPerSecSection1 = 0.629;  
+nullCondition.velocityCmPerSecSection2 = 0.629;
 nullCondition.stimType = condStimTypes(iCond); 
 conditionInfo(iCond).nullCondition = nullCondition;
 
