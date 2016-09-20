6ee5149bfe96a9d00ca50728c27eead963705f81
fa28 <fa28@st-andrews.ac.uk>
2016-09-20 15:40:44 +0100

    trying to get gabor on screen
    commented out rest of code to try and get gabor up on screen.

--- psychMaster/trialFiles/serial_gabor_trial.m
+++ psychMaster/trialFiles/serial_gabor_trial.m
@@ -97,7 +97,7 @@ gabortex = CreateProceduralGabor(window,gaborDimPix,[],...
 propertiesMat = [phase, freq, sigma, contrast, aspectRatio, 0, 0, 0];
 
 %draw the Gabor
-Scree('DrawTextures', window, gabortex, [],[], orientation, [], [], [], [],...
+Screen('DrawTextures', window, gabortex, [],[], orientation, [], [], [], [],...
     kpsychDontDoRotation, propertiesMat');
 
 
@@ -105,199 +105,198 @@ stimStartTime= Screen('Flip',expInfo.curWindow);
 requestedStimEndTime=stimStartTime + conditionInfo.stimDuration;
 actualStimEndTime=Screen('Flip', expInfo.curWindow, requestedStimEndTime);
 
-%%%%%
-getParticipantResponse();
 
-trialData.stimStartTime = stimStartTime;
-trialData.stimEndTime   = actualStimEndTime;
+%getParticipantResponse();
+
+% trialData.stimStartTime = stimStartTime;
+% trialData.stimEndTime   = actualStimEndTime;
 
 
-for iFrame = 1:nFrames
+%for iFrame = 1:nFrames
 %for iFrame = 1
 % 
 %
 %     
 %     %creates a gabor texture. this has to be in the loop beacuse we want to
 %     %create a new gabor on every frame we present.
-     my_gabor = createGabor(radiusPix, sigmaPix, cyclesPerSigma, contrast, phase, orient);
-     my_noise = conditionInfo.noiseSigma.*randn(size(my_gabor));
-     my_noise = max(min(my_noise,.25),-.25);
+%     my_gabor = createGabor(radiusPix, sigmaPix, cyclesPerSigma, contrast, phase, orient);
+ %    my_noise = conditionInfo.noiseSigma.*randn(size(my_gabor));
+   %  my_noise = max(min(my_noise,.25),-.25);
 %     %convert it to a texture 'tex'
-    tex=Screen('makeTexture', expInfo.curWindow, my_gabor+my_noise);
-    Screen('DrawTexture', expInfo.curWindow, tex, [], destRect, [], 0);
-    Screen('DrawLines', expInfo.curWindow, xy,lineWidth,lineColor,expInfo.center);
+  %  tex=Screen('makeTexture', expInfo.curWindow, my_gabor+my_noise);
+   % Screen('DrawTexture', expInfo.curWindow, tex, [], destRect, [], 0);
+    %Screen('DrawLines', expInfo.curWindow, xy,lineWidth,lineColor,expInfo.center);
 % 
 %     
 %     
 %     
-      if expInfo.enablePowermate
-         Screen('DrawingFinished',expInfo.curWindow,expInfo.dontclear);
-         err=PsychHID('ReceiveReports',expInfo.powermateId,options);
-      end
+    %  if expInfo.enablePowermate
+    %     Screen('DrawingFinished',expInfo.curWindow,expInfo.dontclear);
+     %    err=PsychHID('ReceiveReports',expInfo.powermateId,options);
+      %end
 %     
-     flipTimes(iFrame)=Screen('Flip', expInfo.curWindow);
-     WaitSecs(2);
+    % flipTimes(iFrame)=Screen('Flip', expInfo.curWindow);
+    % WaitSecs(2);
 %     
-     if isfield(expInfo,'writeMovie') && expInfo.writeMovie
-         Screen('AddFrameToMovie', expInfo.curWindow,...
-            CenterRect([0 0 1024 1024], Screen('Rect', expInfo.curWindow)));
-    end
+    % if isfield(expInfo,'writeMovie') && expInfo.writeMovie
+     %    Screen('AddFrameToMovie', expInfo.curWindow,...
+       %     CenterRect([0 0 1024 1024], Screen('Rect', expInfo.curWindow)));
+  %  end
 %     
-    if expInfo.enablePowermate
-         err=PsychHID('ReceiveReports',expInfo.powermateId,options);
-         r=PsychHID('GiveMeReports',expInfo.powermateId);
-         if ~isempty(r)
-             lastY = y(end);
-             y =[cat(1,r(:).report)];
-             y = typecast(uint8(y(:,2)),'int8');
-             y = double(y);
-             y = [lastY; y];
-             t = [ 1000*([ lastT r(:).time]-r(1).time) ];
-             lastT = r(end).time;
+  %  if expInfo.enablePowermate
+      %   err=PsychHID('ReceiveReports',expInfo.powermateId,options);
+      %   r=PsychHID('GiveMeReports',expInfo.powermateId);
+        % if ~isempty(r)
+          %   lastY = y(end);
+          %   y =[cat(1,r(:).report)];
+            % y = typecast(uint8(y(:,2)),'int8');
+           %  y = double(y);
+           %  y = [lastY; y];
+           %  t = [ 1000*([ lastT r(:).time]-r(1).time) ];
+           %  lastT = r(end).time;
 %             
-             report(iFrame).r = r;
+            % report(iFrame).r = r;
 %             
-             thisShift = .5*trapz(t,y);
-             totalShift= totalShift-thisShift;            
-         end
-         y = 0;
-         lastT = GetSecs;
+            % thisShift = .5*trapz(t,y);
+            % totalShift= totalShift-thisShift;            
+       %  end
+       %  y = 0;
+        % lastT = GetSecs;
 %         
-         trialData.respOri(iFrame) =  initLineOri+totalShift;
-     else
-         [x,y] = GetMouse(expInfo.curWindow);
-         trialData.mousePos(iFrame,1) = x-xStart;
-         trialData.mousePos(iFrame,2) = y-yStart;
-         trialData.respOri(iFrame) = initLineOri+.5*(x-xStart);
-     end
+       %  trialData.respOri(iFrame) =  initLineOri+totalShift;
+   %  else
+     %    [x,y] = GetMouse(expInfo.curWindow);
+     %    trialData.mousePos(iFrame,1) = x-xStart;
+     %    trialData.mousePos(iFrame,2) = y-yStart;
+      %   trialData.respOri(iFrame) = initLineOri+.5*(x-xStart);
+    % end
 %     
-     trialData.stimOri(iFrame) = orient;
+   %  trialData.stimOri(iFrame) = orient;
      %Rotation matrix; 
-     rotMtx = [cosd(trialData.respOri(iFrame)) -sind(trialData.respOri(iFrame));...
-           sind(trialData.respOri(iFrame)) cosd(trialData.respOri(iFrame))];
-     xy = rotMtx'*initXy;
+   %  rotMtx = [cosd(trialData.respOri(iFrame)) -sind(trialData.respOri(iFrame));...
+     %      sind(trialData.respOri(iFrame)) cosd(trialData.respOri(iFrame))];
+   %  xy = rotMtx'*initXy;
 %     
-     if expInfo.enablePowermate
-         err=PsychHID('ReceiveReports',expInfo.powermateId,options);
-     end
+    % if expInfo.enablePowermate
+      %   err=PsychHID('ReceiveReports',expInfo.powermateId,options);
+  %   end
     %release the texture after we flip because we will redraw again in this
 %     %loop.
-     Screen('Close', tex);
-     if expInfo.useKbQueue
-         [ trialData.pressed, trialData.firstPress]=KbQueueCheck(expInfo.deviceIndex);
-     else
-        [ trialData.pressed, secs, keyCode]=KbCheck(expInfo.deviceIndex);
-        trialData.firstPress = secs*keyCode;
-     end
+    % Screen('Close', tex);
+    % if expInfo.useKbQueue
+    %     [ trialData.pressed, trialData.firstPress]=KbQueueCheck(expInfo.deviceIndex);
+   %  else
+      %  [ trialData.pressed, secs, keyCode]=KbCheck(expInfo.deviceIndex);
+      %  trialData.firstPress = secs*keyCode;
+    % end
 %     
 %     
 %     %Pressed too early.  Abort trial and put in some default values in the
 %     %returned data.
-    if trialData.pressed
-                  trialData.pressed = false;
+  %  if trialData.pressed
+ %                 trialData.pressed = false;
 %                 trialData.firstPress = zeros(size(trialData.firstPress));
-        flipTimes(iFrame)=Screen('Flip', expInfo.curWindow);
-        trialData.flipTimes = flipTimes;
-        trialData.validTrial = false;
-        return;
+ %       flipTimes(iFrame)=Screen('Flip', expInfo.curWindow);
+ %       trialData.flipTimes = flipTimes;
+  %      trialData.validTrial = false;
+  %      return;
 %         
 % 
-    end
+   % end
 %     
 % end
 % 
-Screen('Flip', expInfo.curWindow);
-trialData.validTrial = true;
+% Screen('Flip', expInfo.curWindow);
+% trialData.validTrial = true;
 
 % Finalize and close movie file, if any:
-if isfield(expInfo,'writeMovie') && expInfo.writeMovie
-    Screen('FinalizeMovie', movie);
-end
-curTime = GetSecs;
+% if isfield(expInfo,'writeMovie') && expInfo.writeMovie
+  %  Screen('FinalizeMovie', movie);
+% end
+% curTime = GetSecs;
 
 %Flush any events that happend before the end of the trial
-if expInfo.useKbQueue
-    KbQueueFlush();
-end
+% if expInfo.useKbQueue
+  %  KbQueueFlush();
+% end
 
-trialData.feedbackMsg = [num2str(round(trialData.respOri)) ' degrees'];
+% trialData.feedbackMsg = [num2str(round(trialData.respOri)) ' degrees'];
 
 
 %Reset times to be with respect to trial end.
 %trialData.firstPress = trialData.firstPress-trialData.flipTimes(end);
 
-function = getParticipantResponse()
-           waitingForResponse = true;
-           initLineOri  = 360*rand();
-           totalShift = 0;
-           xStart,yStart] = GetMouse(expInfo.curWindow);
-           y = 0;
+% function  [getParticipantResponse()]
+         %  waitingForResponse = true;
+          % initLineOri  = 360*rand();
+          % totalShift = 0;
+          % xStart,yStart] = GetMouse(expInfo.curWindow);
+          % y = 0;
       
        
         %Rotation matrix;
-        rotMtx = [cosd(initLineOri) -sind(initLineOri);...
-            sind(initLineOri) cosd(initLineOri)];
-        initXy = [0 0; lineLength -lineLength];
-        xy = rotMtx'*initXy;
+       % rotMtx = [cosd(initLineOri) -sind(initLineOri);...
+          %  sind(initLineOri) cosd(initLineOri)];
+       % initXy = [0 0; lineLength -lineLength];
+       % xy = rotMtx'*initXy;
         
-        while waitingForResponse
+       % while waitingForResponse
              
-            if isfield(expInfo,'writeMovie') && expInfo.writeMovie
-                Screen('AddFrameToMovie', expInfo.curWindow,...
-                CenterRect([0 0 1024 1024], Screen('Rect', expInfo.curWindow)));
-end
+          %  if isfield(expInfo,'writeMovie') && expInfo.writeMovie
+            %    Screen('AddFrameToMovie', expInfo.curWindow,...
+             %   CenterRect([0 0 1024 1024], Screen('Rect', expInfo.curWindow)));
+% end
             
-            if expInfo.enablePowermate
-                err=PsychHID('ReceiveReports',expInfo.powermateId,options);
-                r=PsychHID('GiveMeReports',expInfo.powermateId);
-                if ~isempty(r)
-                    lastY = y(end);
-                    y =[cat(1,r(:).report)];
-                    y = typecast(uint8(y(:,2)),'int8');
-                    y = double(y);
-                    y = [lastY; y];
-                    t = [ 1000*([ lastT r(:).time]-r(1).time) ];
-                    lastT = r(end).time;
+       %     if expInfo.enablePowermate
+        %        err=PsychHID('ReceiveReports',expInfo.powermateId,options);
+          %      r=PsychHID('GiveMeReports',expInfo.powermateId);
+            %    if ~isempty(r)
+             %       lastY = y(end);
+              %      y =[cat(1,r(:).report)];
+                 %   y = typecast(uint8(y(:,2)),'int8');
+                %    y = double(y);
+                %    y = [lastY; y];
+                %    t = [ 1000*([ lastT r(:).time]-r(1).time) ];
+                  %  lastT = r(end).time;
                     
-                    report(iFrame).r = r;
+                 %   report(iFrame).r = r;
                     
-                    thisShift = .5*trapz(t,y);
-                    totalShift= totalShift-thisShift;
-                end
-                y = 0;
-                lastT = GetSecs;
+                   % thisShift = .5*trapz(t,y);
+                   % totalShift= totalShift-thisShift;
+              %  end
+             %   y = 0;
+              %  lastT = GetSecs;
                 
-                thisOrient =  initLineOri+totalShift;
-            else %use the mouse
-                [x,y,buttons] = GetMouse(expInfo.curWindow);
+               % thisOrient =  initLineOri+totalShift;
+          %  else %use the mouse
+             %   [x,y,buttons] = GetMouse(expInfo.curWindow);
                 
-                if any(buttons); %Ok got a response lets quit
-                    waitingForResponse = false;
-                    trialData.responseTime = GetSecs;
-                else
+              %  if any(buttons); %Ok got a response lets quit
+                %    waitingForResponse = false;
+                 %   trialData.responseTime = GetSecs;
+               % else
                     thisOrient = initLineOri+.5*(x-xStart);
-                end
+             %   end
                 
-            end
+         %   end
 
          
             %Rotation matrix;
-            rotMtx = [cosd(thisOrient) -sind(thisOrient);...
-                sind(thisOrient) cosd(thisOrient)];
-            xy = rotMtx'*initXy;
+           % rotMtx = [cosd(thisOrient) -sind(thisOrient);...
+            %    sind(thisOrient) cosd(thisOrient)];
+          %  xy = rotMtx'*initXy;
             
-            Screen('DrawLines', expInfo.curWindow, xy,lineWidth,lineColor,expInfo.center,1);
+          %  Screen('DrawLines', expInfo.curWindow, xy,lineWidth,lineColor,expInfo.center,1);
             
-            Screen('Flip', expInfo.curWindow);
+          %  Screen('Flip', expInfo.curWindow);
             
             
         
-        end
+     %   end
         
-        trialData.respOri = thisOrient;
- end 
-end 
-
+     %   trialData.respOri = thisOrient;
+% end 
+% end
 
 
 
