46b9f225646634697d091c8303e06bbe3bd3927e
fa28 <fa28@st-andrews.ac.uk>
2016-09-28 12:23:08 +0100

    trying to fix

--- psychMaster/trialFiles/serial_gabor_trial.m
+++ psychMaster/trialFiles/serial_gabor_trial.m
@@ -83,20 +83,6 @@ Screen('DrawTextures', window, gabortex, [],[], orientation, [], [], [], [],...
    
    
 
-% Build a procedural gabor texture (Note: to get a "standard" Gabor patch
-% we set a grey background offset, disable normalisation, and set a
-% pre-contrast multiplier of 0.5.
-% For full details see:
-% https://groups.yahoo.com/neo/groups/psychtoolbox/conversations/topics/9174
-% backgroundOffset = [0.5 0.5 0.5 0.0];
-% disableNorm = 1;
-% preContrastMultiplier = 0.5;
-% gabortex = CreateProceduralGabor(ExpInfo.Curwindow, gaborDimPix, gaborDimPix, [],...
-%     backgroundOffset, disableNorm, preContrastMultiplier);
-% 
-% % Randomise the phase of the Gabors and make a properties matrix.
-% propertiesMat = [phase, freq, sigma, contrast, aspectRatio, 0, 0, 0];
-% 
 
 
 stimStartTime= Screen('Flip',expInfo.curWindow);
@@ -105,7 +91,7 @@ actualStimEndTime=Screen('Flip', expInfo.curWindow, requestedStimEndTime);
 
 Screen('Flip', window);
 
-kbStrokeWait
+KbStrokeWait
 
 end
 
