24b6f7fe4af0aa5a66b7b2826c703c59866f4f14
fa28 <fa28@st-andrews.ac.uk>
2016-09-28 11:27:42 +0100

    should work now
    would work now

--- psychMaster/trialFiles/serial_gabor_trial.m
+++ psychMaster/trialFiles/serial_gabor_trial.m
@@ -82,39 +82,21 @@ end
    %Screen('DrawLines', expInfo.curWindow, xy,lineWidth,lineColor,expInfo.center);
    
    
-%code that draws gabor
-% tried using procedural gabor from psychtoolbox
-
-% Dimension of the region where will draw the Gabor in pixels
-gaborDimPix = windowRect(4) / 2;
-
-% Sigma of Gaussian
-sigma = gaborDimPix / 7;
-
-% Obvious Parameters
-orientation = 0;
-contrast = 0.8;
-aspectRatio = 1.0;
-phase = 0;
-
-%Spatial Frequency (Cycles Per Pixel) One Cycle = Grey-Black-Grey-White-Grey i.e. One Black and One White Lobe
-numCycles = 5;
-freq = numCycles / gaborDimPix;
 
 % Build a procedural gabor texture (Note: to get a "standard" Gabor patch
 % we set a grey background offset, disable normalisation, and set a
 % pre-contrast multiplier of 0.5.
 % For full details see:
 % https://groups.yahoo.com/neo/groups/psychtoolbox/conversations/topics/9174
-backgroundOffset = [0.5 0.5 0.5 0.0];
-disableNorm = 1;
-preContrastMultiplier = 0.5;
-gabortex = CreateProceduralGabor(window, gaborDimPix, gaborDimPix, [],...
-    backgroundOffset, disableNorm, preContrastMultiplier);
-
-% Randomise the phase of the Gabors and make a properties matrix.
-propertiesMat = [phase, freq, sigma, contrast, aspectRatio, 0, 0, 0];
-
+% backgroundOffset = [0.5 0.5 0.5 0.0];
+% disableNorm = 1;
+% preContrastMultiplier = 0.5;
+% gabortex = CreateProceduralGabor(ExpInfo.Curwindow, gaborDimPix, gaborDimPix, [],...
+%     backgroundOffset, disableNorm, preContrastMultiplier);
+% 
+% % Randomise the phase of the Gabors and make a properties matrix.
+% propertiesMat = [phase, freq, sigma, contrast, aspectRatio, 0, 0, 0];
+% 
 
 
 stimStartTime= Screen('Flip',expInfo.curWindow);
