91d13174db86c4045d243b7fb355154e5d265d68
fa28 <fa28@st-andrews.ac.uk>
2016-10-18 13:03:57 +0100

    changed trial no
    trial no change

--- psychMaster/paradigmFiles/psychparadigm_serial_gabor.m
+++ psychMaster/paradigmFiles/psychparadigm_serial_gabor.m
@@ -43,23 +43,23 @@ conditionInfo(1).freq              =1; %frequency of the gabor in cycles per sig
 conditionInfo(1).nReps             = 104; %% number of trials to present this condition. 
 conditionInfo(1).stimRadiusDeg  = 6;    %stimulus size in degree;
 
-conditionInfo(1).contrast = 0.25;
+conditionInfo(1).contrast = 0.15;
 conditionInfo(1).noiseSigma = .15;
 conditionInfo(1).orientationSigma = 5;
 %Implement arbitrary forward models. 
 %conditionInfo(1).forwardModel = [ 1 0 ]; %Forward model
-conditionInfo(1).label = 'Contrast: 0.25'
+conditionInfo(1).label = 'Contrast: 0.15';
 
 
-% conditionInfo(2) = conditionInfo(1);
-% conditionInfo(2).orientationSigma = 5;
-% conditionInfo(2).contrast = 0.04 ;
-% conditionInfo(2).label = 'Contrast: 0.04'
-% 
-% conditionInfo(3) = conditionInfo(1);
-% conditionInfo(3).orientationSigma = 5;
-% conditionInfo(3).contrast = 0.08;
-% conditionInfo(3).label = 'Contrast: 0.08'
+conditionInfo(2) = conditionInfo(1);
+conditionInfo(2).orientationSigma = 5;
+conditionInfo(2).contrast = 0.04 ;
+conditionInfo(2).label = 'Contrast: 0.04'
+
+conditionInfo(3) = conditionInfo(1);
+conditionInfo(3).orientationSigma = 5;
+conditionInfo(3).contrast = 0.08;
+conditionInfo(3).label = 'Contrast: 0.08'
 
 
 
