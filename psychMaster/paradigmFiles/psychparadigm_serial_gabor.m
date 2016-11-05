9f8b9fd42f1533eaa0df9fd32215f5a1c3648ac7
fa28 <fa28@st-andrews.ac.uk>
2016-11-05 15:51:55 +0000

    trials nos

--- psychMaster/paradigmFiles/psychparadigm_serial_gabor.m
+++ psychMaster/paradigmFiles/psychparadigm_serial_gabor.m
@@ -42,7 +42,7 @@ conditionInfo(1).responseDuration  = 0;    %Post trial window for waiting for a 
 
 conditionInfo(1).sigma             =2; %standard deviation of the gabor in degrees
 conditionInfo(1).freq              =1; %frequency of the gabor in cycles per sigma. 
-conditionInfo(1).nReps             = 20; %% number of trials to present this condition. 
+conditionInfo(1).nReps             = 104; %% number of trials to present this condition. 
 conditionInfo(1).stimRadiusDeg     =   6;    %stimulus size in degree;
 
 conditionInfo(1).contrast = 0.05;
