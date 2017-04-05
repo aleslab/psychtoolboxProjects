426148e6ae18fcdb05c2900c9d123768d09bcb12
Justin Ales <justin.ales@gmail.com>
2017-04-05 11:24:33 +0100

    Added code to show cursor when testing conditions on 2nd monitor.

--- psychMaster/functionLibrary/GUI/pmGui.m
+++ psychMaster/functionLibrary/GUI/pmGui.m
@@ -66,7 +66,12 @@ end
 % Update handles structure
 guidata(hObject, handles);
 
+%When the GUI is active we should enable the keyboard and the mouse
 ListenChar(0);
+if isfield(handles.expInfo, 'screenNum')
+    ShowCursor(handles.expInfo.screenNum);
+end
+
 if ispref('psychMaster','lastParadigmFile')
     lastParadigmFile = getpref('psychMaster','lastParadigmFile');
 else
@@ -203,7 +208,15 @@ function runExperimentBtn_Callback(hObject, eventdata, handles)
 handles.sessionInfo.returnToGui = false;
 handles.sessionInfo.userCancelled = false;
 guidata(hObject,handles);
+
+%Don't echo keypresses when really running experiment
 ListenChar(2);
+%If we're running full screen lets hide the mouse cursor from view.
+if handles.expInfo.useFullScreen == true
+    HideCursor(handles.expInfo.screenNum);
+end
+
+
 uiresume(handles.pmGuiParentFig);
 
 % --- Executes on button press in cancelBtn.
