27423f09cbf5e2bfdc64edbcd9b5a5804fb56f3f
Justin Ales <justin.ales@gmail.com>
2017-04-20 15:51:27 +0100

    Fixed another bug with hiding cursor.

--- psychMaster/psychMaster.m
+++ psychMaster/psychMaster.m
@@ -306,7 +306,7 @@ try
     %Need to do this here for different OS versions and to enable 
     %control from pmGui
     if expInfo.useFullScreen == true
-        HideCursor(handles.expInfo.screenNum);
+        HideCursor(expInfo.screenNum);
     end
 
     
@@ -337,7 +337,7 @@ try
         %Need to do this here for different OS versions and to enable
         %control from pmGui
         if expInfo.useFullScreen == true
-            HideCursor(handles.expInfo.screenNum);
+            HideCursor(expInfo.screenNum);
         end
         %This function handles everything for the experimental trials.
         mainExperimentLoop();
