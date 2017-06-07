8f56f9715b10610097fcb7cf907102c05d98d99d
abigailrilee <abigailrilee@googlemail.com>
2017-06-07 16:51:37 +0100

    Modifying how LateralLineTrial works
    based off of speed rather than frames

--- psychMaster/paradigmFiles/Motion/temporal_integration/lateralLine_gap_zero.m
+++ psychMaster/paradigmFiles/Motion/temporal_integration/lateralLine_gap_zero.m
@@ -3,11 +3,11 @@ function [conditionInfo, expInfo] = lateralLine_gap_zero(expInfo)
 expInfo = lateralLineDefaultSettings(expInfo);
 expInfo.paradigmName = 'lateralLine_gap_zero';
 
-section2velocity = [16 32]; %velocities to use in cm/s; picked randomly for now
+section2velocity = [0.5 2]; %velocities to use in cm/s; picked randomly for now
 
 for iCond = 1: length(section2velocity);
     conditionInfo(iCond).trialFun=@LateralLineTrial; %This defines what function to call to draw the condition
-    conditionInfo(iCond).type = 'generic'; %type of task based on ptbCorgi definition
+    conditionInfo(iCond).type = 'simpleResponse'; %type of task based on ptbCorgi definition
     conditionInfo(iCond).label = ['zero_gap_' num2str(section2velocity(iCond))]; 
     %the labels for the levels when viewing in ptbCorgi gui
     conditionInfo(iCond).nReps = 10; %number of repeats of the level
@@ -20,10 +20,10 @@ for iCond = 1: length(section2velocity);
     %in seconds that people will have between section 1 and section 2
     conditionInfo(iCond).responseDuration = 3;    %How long participants will have to respond
     
-    conditionInfo(iCond).velocityCmPerSecSection1 = 10; % velocity of section 1 in cm/s. Constant for this exp
-    conditionInfo(iCond).velocityCmPerSecSection2 = section2velocity(iCond); %velocity of section 2 in cm/s
-    
-    conditionInfo(iCond).startPos = -4; %the start position of the line on the screen in cm 
+    conditionInfo(iCond).velocityDegPerSecSection1 = 1; % velocity of section 1 in deg/s. Constant for this exp
+    conditionInfo(iCond).velocityDegPerSecSection2 = section2velocity(iCond); %velocity of section 2 in deg/s
+    conditionInfo(iCond).gapVelocity = 0; %velocity for temporal gap
+    conditionInfo(iCond).startPos = -1; %the start position of the line on the screen in cm 
     %number picked at random for now, will likely be changed later.
     %Negative = left hand side of the screen.
 
--- psychMaster/trialFiles/Motion/LateralLineTrial.m
+++ psychMaster/trialFiles/Motion/LateralLineTrial.m
@@ -37,64 +37,84 @@ trialData.nFrames.Section2 = nFramesSection2;
 trialData.nFrames.Total = nFramesTotal;
 
 %cm/frame velocity
-velCmPerFrameSection1  = conditionInfo.velocityCmPerSecSection1*expInfo.ifi;
-velCmPerFrameSection2  = conditionInfo.velocityCmPerSecSection2*expInfo.ifi;
+% velCmPerFrameSection1  = conditionInfo.velocityCmPerSecSection1*expInfo.ifi;
+% velCmPerFrameSection2  = conditionInfo.velocityCmPerSecSection2*expInfo.ifi;
+
+velSection1PixPerSec = conditionInfo.velocityDegPerSecSection1*expInfo.ppd;
+velSection2PixPerSec = conditionInfo.velocityDegPerSecSection2*expInfo.ppd;
+gapVelocityPixPerSec = conditionInfo.gapVelocity*expInfo.ppd;
 
 trialData.flipTimes = NaN(nFramesTotal,1);
 frameIdx = 1;
 
 %% Creating the line and the structure of the stimulus presentation
 
-pixelStartPos = round(expInfo.center(1) + (expInfo.pixPerCm * conditionInfo.startPos));
+pixelStartPos = (expInfo.center(1) + round(expInfo.ppd * conditionInfo.startPos));
 %finding the pixel start position relative to the centre of the screen
 %(left of screen centre is - and right is +) converting from the cm value
 %given in the paradigm file
 
 currLinePos = pixelStartPos;
 
-for iFrame = 1:nFramesPreStim %during the pre stimulus duration
+%drawing the line at the current line position (in the x axis) from the
+%top to the bottom of the screen
+Screen('DrawLines', expInfo.curWindow, [currLinePos, currLinePos; 0, screenYpixels], expInfo.lw);
+expInfo = drawFixation(expInfo, fixationInfo);
+LAT = Screen('Flip', expInfo.curWindow,vbl+expInfo.ifi/2); %line appearance time
+trialData.flipTimes(frameIdx) = vbl;
+frameIdx = frameIdx+1;
+
+currentTime = LAT;
+preStimEndTime = LAT + conditionInfo.preStimDuration;
+section1endtime = preStimEndTime + conditionInfo.stimDurationSection1;
+gapendtime = section1endtime + conditionInfo.temporalGap;
+section2endtime = gapendtime + conditionInfo.stimDurationSection2;
+
+nextFlipTime = LAT+expInfo.ifi/2;
+currIfi = expInfo.ifi;
+previousFlipTime = LAT;
+velocityPixPerSec = 0;
+waitTime = 0;
+while currentTime < section2endtime
     
-    %drawing the line at the current line position (in the x axis) from the
-    %top to the bottom of the screen
-    Screen('DrawLines', expInfo.curWindow, [currLinePos, currLinePos; 0, screenYpixels], expInfo.lw);
+    if currentTime < preStimEndTime
+        velocityPixPerSec = 0;
+        drawLine = true;
+        
+    elseif currentTime > preStimEndTime && currentTime < section1endtime
+        velocityPixPerSec = velSection1PixPerSec;
+        drawLine = true;
+        
+    elseif currentTime > section1endtime && currentTime < gapendtime
+        velocityPixPerSec = gapVelocityPixPerSec;
+        drawLine = false;
+        
+    elseif currentTime > gapendtime && currentTime < section2endtime
+        velocityPixPerSec = velSection2PixPerSec;
+        drawLine = true;
+        
+    else
+        velocityPixPerSec = velocityPixPerSec;
+        drawLine = true;
+        
+    end
+    
+    currLinePos = currLinePos + velocityPixPerSec * currIfi; %currLinePos is in pixels per frame
+    
+    if drawLine == true
+        
+        Screen('DrawLines', expInfo.curWindow, [currLinePos, currLinePos; 0, screenYpixels], expInfo.lw);
+        
+    end
     expInfo = drawFixation(expInfo, fixationInfo);
-    vbl=Screen('Flip', expInfo.curWindow,vbl+expInfo.ifi/2);
-    trialData.flipTimes(frameIdx) = vbl;
+    currentFlipTime = Screen('Flip', expInfo.curWindow,nextFlipTime);
+    trialData.flipTimes(frameIdx) = currentFlipTime;
     frameIdx = frameIdx+1;
+    nextFlipTime = currentFlipTime + expInfo.ifi/2;
+    currentTime = currentFlipTime;
+    currIfi = currentFlipTime - previousFlipTime;
+    previousFlipTime = currentFlipTime;   
+
 end
-
-for iFrame = 1:nFramesSection1
-    
-    Screen('DrawLines', expInfo.curWindow, [currLinePos, currLinePos; 0, screenYpixels], expInfo.lw);
-    expInfo = drawFixation(expInfo, fixationInfo);
-    vbl=Screen('Flip', expInfo.curWindow,vbl+expInfo.ifi/2);
-    trialData.flipTimes(frameIdx) = vbl;
-    frameIdx = frameIdx+1;
-    
-    currLinePos = currLinePos + velCmPerFrameSection1;
-end
-
-if conditionInfo.temporalGap > 0
-    
-    expInfo = drawFixation(expInfo, fixationInfo);
-    vbl=Screen('Flip', expInfo.curWindow,vbl+expInfo.ifi/2);
-    WaitSecs(conditionInfo.temporalGap);
-    
-end
-
-for iFrame = 1:nFramesSection2
-    
-    Screen('DrawLines', expInfo.curWindow, [currLinePos, currLinePos; 0, screenYpixels], expInfo.lw);
-    expInfo = drawFixation(expInfo, fixationInfo);
-    vbl=Screen('Flip', expInfo.curWindow,vbl+expInfo.ifi/2);
-    trialData.flipTimes(frameIdx) = vbl;
-    frameIdx = frameIdx+1;
-    
-    currLinePos = currLinePos + velCmPerFrameSection2;
-    
-end
-
-
-
 
 end
