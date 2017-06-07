14eca0d71bcdf7f077368d87d93d5da7030f9b21
abigailrilee <abigailrilee@googlemail.com>
2017-06-07 21:55:54 +0100

    Minor edits to the temporal integration paradigm file

--- psychMaster/paradigmFiles/Motion/temporal_integration/lateralLine_gap.m
+++ psychMaster/paradigmFiles/Motion/temporal_integration/lateralLine_gap.m
@@ -1,21 +1,22 @@
 function [conditionInfo, expInfo] = lateralLine_gap(expInfo)
 
 expInfo = lateralLineDefaultSettings(expInfo);
-expInfo.paradigmName = 'lateralLine_gap_zero';
-expInfo.viewingDistance = 92; %in cm I need to check this
+expInfo.paradigmName = 'lateralLine_gap';
+expInfo.viewingDistance = 92; %in cm; I need to check this
 expInfo.instructions = 'Did the line speed up?';
 expInfo.pauseInfo = 'Paused\nPress any key to continue';
 
 section2velocity = [0.5 2]; %velocities to use in deg/s; picked randomly for now
 
 for iCond = 1: length(section2velocity);
+    %general
     conditionInfo(iCond).trialFun=@LateralLineTrial; %This defines what function to call to draw the condition
     conditionInfo(iCond).type = 'simpleResponse'; %type of task based on ptbCorgi definition
-    conditionInfo(iCond).label = ['zero_gap_' num2str(section2velocity(iCond))]; 
+    conditionInfo(iCond).label = ['temporal_Integration_' num2str(section2velocity(iCond))]; 
     %the labels for the levels when viewing in ptbCorgi gui
     conditionInfo(iCond).nReps = 10; %number of repeats of the level
-    conditionInfo(iCond).isNullCorrect = false; %so the standard isn't the "correct" answer
     
+    %timings
     conditionInfo(iCond).preStimDuration  = 0.25;  %Static time before stimulus change
     conditionInfo(iCond).stimDurationSection1 = 0.50; %approximate stimulus duration in seconds
     conditionInfo(iCond).stimDurationSection2 = 0.50; %variable in future but for now fixed
@@ -23,6 +24,7 @@ for iCond = 1: length(section2velocity);
     %in seconds that people will have between section 1 and section 2
     conditionInfo(iCond).responseDuration = 3;    %How long participants will have to respond
     
+    %velocities
     conditionInfo(iCond).velocityDegPerSecSection1 = 1; % velocity of section 1 in deg/s. Constant for this exp
     conditionInfo(iCond).velocityDegPerSecSection2 = section2velocity(iCond); %velocity of section 2 in deg/s
     conditionInfo(iCond).gapVelocity = 0; %velocity in deg/s for temporal gap
