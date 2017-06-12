ffa2529b2aaa3e10b728841931e2a13c8d267e12
Marlene <marlene.poncet@gmail.com>
2017-06-12 13:56:46 +0100

    add task to LR paradigm
    nb of changes in luminance

--- psychMaster/paradigmFiles/psychParadigm_longRange.m
+++ psychMaster/paradigmFiles/psychParadigm_longRange.m
@@ -1,5 +1,5 @@
 function [conditionInfo, expInfo] = psychParadigm_longRange(expInfo)
-
+% fix iti? why not 500+500*rand(1)
 
 %paradigmName is what will be prepended to data files
 expInfo.paradigmName = 'longRange';
@@ -13,61 +13,68 @@ expInfo.fixationInfo(1).size  = .2;
 expInfo.fixationInfo(1).lineWidthPix = 2;
 expInfo.fixationInfo(1).color = 0;
 
-expInfo.instructions = 'press any key to go to the next trial ';
+expInfo.instructions = 'count the number of dims';
 
 %% General conditions
-conditionInfo(1).iti = 0.5;
-conditionInfo(1).nReps = 2;
+conditionInfo(1).iti = 0.5; 
+conditionInfo(1).nReps = 2; %30 should make it around 60min 
 conditionInfo(1).type = 'Generic';
 conditionInfo(1).giveFeedback = 0;
 conditionInfo(1).giveAudioFeedback = 0;
 conditionInfo(1).intervalBeep = 0;
+conditionInfo(1).maxToAnswer = 2; % max time to answer
 % conditionInfo(1).randomizeField = 'false';
 
 %% stimulus
 conditionInfo(1).stimSize = [0 0 100 100];
-conditionInfo(1).nFramesPerStim = 12;
+conditionInfo(1).nFramesPerStim = 10; % at 60Hz refresh = 6 img/sec
+conditionInfo(1).trialDuration = 6; % 12 sec
+conditionInfo(1).totFlip = 6 * conditionInfo(1).trialDuration; %80 for 60Hz 12sec trial
 conditionInfo(1).trialFun=@trial_longRange;
 conditionInfo(1).movingStep = 0;
+conditionInfo(1).motion = 0; % by default, no motion 
 
 % same parameters in all conditions
-conditionInfo(2) = conditionInfo(1);
-conditionInfo(3) = conditionInfo(1);
-conditionInfo(4) = conditionInfo(1);
-conditionInfo(5) = conditionInfo(1);
-conditionInfo(6) = conditionInfo(1);
+for cc=2:9
+    conditionInfo(cc) = conditionInfo(1);
+end
 
 %% experimental manipulation
+
+% first 4 are long range
+for cc=1:4
+    conditionInfo(cc).xloc = 2; % division where the stim will be presented horizontally = centerx/2
+end
 conditionInfo(1).label = 'long range';
 conditionInfo(1).sideStim = 'both';
-conditionInfo(1).xloc = 2; % division where the stim will be presented horizontally = centerx/2
 conditionInfo(1).motion = 1;
+conditionInfo(2).label = 'long left';
+conditionInfo(2).sideStim = 'left';
+conditionInfo(3).label = 'long right';
+conditionInfo(3).sideStim = 'right';
+conditionInfo(4).label = 'long simult';
+conditionInfo(4).sideStim = 'both';
 
-conditionInfo(2).label = 'short range';
-conditionInfo(2).sideStim = 'both';
-conditionInfo(2).xloc = 15; % = centerx/8
-conditionInfo(2).motion = 1;
-
-conditionInfo(3).label = 'single left';
-conditionInfo(3).sideStim = 'left';
-conditionInfo(3).xloc = 2;
-conditionInfo(3).motion = 0;
-
-conditionInfo(4).label = 'single right';
-conditionInfo(4).sideStim = 'right';
-conditionInfo(4).xloc = 2;
-conditionInfo(4).motion = 0;
-
-conditionInfo(5).label = 'long simult';
+% 5:8 are long range
+for cc=5:8
+    conditionInfo(cc).xloc = 12; % = centerx/8
+end
+conditionInfo(5).label = 'short range';
 conditionInfo(5).sideStim = 'both';
-conditionInfo(5).xloc = 2;
-conditionInfo(5).motion = 0;
+conditionInfo(5).motion = 1;
+conditionInfo(6).label = 'short left';
+conditionInfo(6).sideStim = 'left';
+conditionInfo(7).label = 'short right';
+conditionInfo(7).sideStim = 'right';
+conditionInfo(8).label = 'short simult';
+conditionInfo(8).sideStim = 'both';
 
-conditionInfo(6).label = 'sweep';
-conditionInfo(6).sideStim = 'both';
-conditionInfo(6).xloc = [15 2]; % where it starts and ends
-conditionInfo(6).motion = 1;
-conditionInfo(6).movingStep = 5;
+% last condition: sweep
+conditionInfo(9).label = 'sweep';
+conditionInfo(9).sideStim = 'both';
+conditionInfo(9).xloc = [15 2]; % where it starts and ends
+conditionInfo(9).motion = 1;
+conditionInfo(9).movingStep = 5;
 
 % expInfo.center does NOT exist (at the beginning) so the following won't work
 % conditionInfo(1).ycoord = expInfo.center(2)/2; 
--- psychMaster/trialFiles/trial_longRange.m
+++ psychMaster/trialFiles/trial_longRange.m
@@ -1,61 +1,191 @@
 function [trialData] = trial_longRange(expInfo, conditionInfo)
+% pb short simultaneous: too close!!
+% give feedback for correct/incorrect answer?
+
+% if a key is pressed during a trial, it stops the trial and becomes
+% invalid
+% if the escape key is pressed then the experiment is aborted
 
 drawFixation(expInfo, expInfo.fixationInfo);
 t = Screen('Flip', expInfo.curWindow);
 trialData.validTrial = true;
 trialData.abortNow   = false;
-trialData.trialStartTime = t; 
+trialData.trialStartTime = t;
+trialData.response = 999;
 
 black = BlackIndex(expInfo.curWindow);
+dimColour = 0.3;
 
+% parameters for the task
+trialData.dims = randi(6,1)-1; % number of dims for this trial
+trialData.flipDim = [11 12 13];
+% determine which flip with dim, 2 dims should not follow each other on the
+% same stimulus
+if conditionInfo.motion 
+    trialData.flipDim = randsample(1:3:(conditionInfo.totFlip),trialData.dims)-1; % Attention 1st flip = 0
+    % was first planning to only avoid dim+3 but seems complicated, here it
+    % only allows a dim every 3 frames
+%     while ismember(1,diff(trialData.flipDim)==1) % do it again until there is no 2 successive flips
+%         trialData.flipDim = sort(randperm(conditionInfo.totFlip+1,trialData.dims)-1); % if motion then any flip BUT avoid 2 successive flips + Attention 1st flip = 0
+%     end
+else % if no motion then it has to be when the stim is on (only odd numbers) + avoid successive dims
+    trialData.flipDim = randsample(1:4:(conditionInfo.totFlip),trialData.dims)-1; % Attention 1st flip = 0
+end
+
+% stim presentation parameters
 rectCircle = conditionInfo.stimSize;
 nbFrames = conditionInfo.nFramesPerStim;
 ifi = expInfo.ifi;
 ycoord = expInfo.center(2)/2;
 xcoord = expInfo.center(1)/conditionInfo.xloc(1);
-if strcmp(conditionInfo.label,'sweep')
-    xcoordEnd = expInfo.center(1)/conditionInfo.xloc(2);
-    movingStep = conditionInfo.movingStep;
-else
-    xcoordEnd = expInfo.center(1)/conditionInfo.xloc(1);
+movingStep = conditionInfo.movingStep;
+if strcmp(conditionInfo.sideStim,'left')
+    xcoordSingle = expInfo.center(1)-xcoord;
+elseif strcmp(conditionInfo.sideStim,'right')
+    xcoordSingle = expInfo.center(1)+xcoord;
 end
+% % only required if the trial ends depending on the position of the stimulus
+% % (mainly for the sweep condition)
+% if strcmp(conditionInfo.label,'sweep')
+%     xcoordEnd = expInfo.center(1)/conditionInfo.xloc(2);
+% else
+%     xcoordEnd = expInfo.center(1)/conditionInfo.xloc(1);
+% end
 
-while ~KbCheck && xcoord<xcoordEnd+1
-    if strcmp(conditionInfo.sideStim,'both')
-        if conditionInfo.motion == 1
+% check the nb of flip
+totFlip = 0;
+
+
+% presentation stimulus
+while ~KbCheck && t<conditionInfo.trialDuration+trialData.trialStartTime-ifi/2 % && xcoord<xcoordEnd+1
+    if strcmp(conditionInfo.sideStim,'both') % 2 stim presented
+        if conditionInfo.motion == 1 % in motion
+            % check if the flip is dim
+            if ismember(totFlip,trialData.flipDim)
+                colStim = dimColour;
+            else
+                colStim = black;
+            end
             drawFixation(expInfo, expInfo.fixationInfo);
-            Screen('FillOval', expInfo.curWindow, black,CenterRectOnPoint(rectCircle,expInfo.center(1)-xcoord,ycoord));
-            t = Screen('Flip', expInfo.curWindow, t + (nbFrames - 0.5) * ifi);
+            Screen('FillOval', expInfo.curWindow, colStim,CenterRectOnPoint(rectCircle,expInfo.center(1)-xcoord,ycoord));
+            t = Screen('Flip', expInfo.curWindow, t + nbFrames * ifi - ifi/2);
+            % trialData.trialStartTime - t
+            totFlip = totFlip+ 1;
+            if totFlip == 1
+                trialData.stimStartTime = t;
+            end
+            % check if the flip is dim
+            if ismember(totFlip,trialData.flipDim)
+                colStim = dimColour;
+            else
+                colStim = black;
+            end
             drawFixation(expInfo, expInfo.fixationInfo);
-            Screen('FillOval', expInfo.curWindow, black,CenterRectOnPoint(rectCircle,expInfo.center(1)+xcoord,ycoord));
-            t = Screen('Flip', expInfo.curWindow, t + (nbFrames - 0.5) * ifi);
+            Screen('FillOval', expInfo.curWindow, colStim,CenterRectOnPoint(rectCircle,expInfo.center(1)+xcoord,ycoord));
+            t = Screen('Flip', expInfo.curWindow, t + nbFrames * ifi - ifi/2);
+            % trialData.trialStartTime - t
             if strcmp(conditionInfo.label,'sweep')
                 xcoord = xcoord + movingStep;
             end
-        else
+            totFlip = totFlip+ 1;
+        else % simultaneous condition
+            % check if the flip is dim
+            if ismember(totFlip,trialData.flipDim)
+                colStim = Shuffle([dimColour,black]);
+            else
+                colStim = [black black];
+            end
             drawFixation(expInfo, expInfo.fixationInfo);
-            Screen('FillOval', expInfo.curWindow, black,CenterRectOnPoint(rectCircle,expInfo.center(1)-xcoord,ycoord));
-            Screen('FillOval', expInfo.curWindow, black,CenterRectOnPoint(rectCircle,expInfo.center(1)+xcoord,ycoord));
-            t = Screen('Flip', expInfo.curWindow, t + (nbFrames - 0.5) * ifi);
+            Screen('FillOval', expInfo.curWindow, colStim(1),CenterRectOnPoint(rectCircle,expInfo.center(1)-xcoord,ycoord));
+            Screen('FillOval', expInfo.curWindow, colStim(2),CenterRectOnPoint(rectCircle,expInfo.center(1)+xcoord,ycoord));
+            t = Screen('Flip', expInfo.curWindow, t + nbFrames * ifi - ifi/2);
+            totFlip = totFlip+ 1;
+            if totFlip == 1
+                trialData.stimStartTime = t;
+            end
             drawFixation(expInfo, expInfo.fixationInfo);
-            t = Screen('Flip', expInfo.curWindow, t + (nbFrames - 0.5) * ifi);
+            t = Screen('Flip', expInfo.curWindow, t + nbFrames * ifi - ifi/2);
+            totFlip = totFlip+ 1;
         end
-    elseif strcmp(conditionInfo.sideStim,'left')
+    else  % only one stim (left or right)
+        % check if the flip is dim
+        if ismember(totFlip,trialData.flipDim)
+            colStim = dimColour;
+        else
+            colStim = black;
+        end
         drawFixation(expInfo, expInfo.fixationInfo);
-        Screen('FillOval', expInfo.curWindow, black,CenterRectOnPoint(rectCircle,expInfo.center(1)-xcoord,ycoord));
-        t = Screen('Flip', expInfo.curWindow, t + (nbFrames - 0.5) * ifi);
+        Screen('FillOval', expInfo.curWindow, colStim,CenterRectOnPoint(rectCircle,xcoordSingle,ycoord));
+        t = Screen('Flip', expInfo.curWindow, t + nbFrames * ifi - ifi/2);
+        totFlip = totFlip+ 1;
+        if totFlip == 1
+            trialData.stimStartTime = t;
+        end
         drawFixation(expInfo, expInfo.fixationInfo);
-        t = Screen('Flip', expInfo.curWindow, t + (nbFrames - 0.5) * ifi);
-    elseif strcmp(conditionInfo.sideStim,'right')
-        drawFixation(expInfo, expInfo.fixationInfo);
-        Screen('FillOval', expInfo.curWindow, black,CenterRectOnPoint(rectCircle,expInfo.center(1)+xcoord,ycoord));
-        t = Screen('Flip', expInfo.curWindow, t + (nbFrames - 0.5) * ifi);
-        drawFixation(expInfo, expInfo.fixationInfo);
-        t = Screen('Flip', expInfo.curWindow, t + (nbFrames - 0.5) * ifi);
+        t = Screen('Flip', expInfo.curWindow, t + nbFrames * ifi - ifi/2);
+        totFlip = totFlip+ 1;
     end
 end
 
-trialData.trialEndTime = t; 
+trialData.stimEndTime = t;
+
+% abort
+[keyIsDown, secs, keyCode]=KbCheck(expInfo.deviceIndex);
+if keyIsDown
+    trialData.validTrial = false;
+    if keyCode(KbName('escape'))
+        trialData.abortNow   = true;
+    end
+end
+
+
+
+% Find the key values (not the same in PC and MAC) for the loop in the response
+for keyVal=0:5
+    vectKeyVal(keyVal+1) = KbName(num2str(keyVal));
+end
+
+
+trialData.totFlip = totFlip;
+if totFlip ~= conditionInfo.totFlip
+    trialData.validTrial = false;
+else
+    % response screen
+    Screen('DrawText', expInfo.curWindow, 'Nb of dims?', expInfo.center(1), expInfo.center(2), [0 0 0]);
+    Screen('DrawText', expInfo.curWindow, '(0-5)', expInfo.center(1), expInfo.center(2)+expInfo.center(2)/4, [0 0 0]);
+    trialData.respScreenTime =Screen('Flip',expInfo.curWindow);
+    while trialData.response==999 && (GetSecs < trialData.respScreenTime + conditionInfo.maxToAnswer -ifi/2)
+        [keyDown, secs, keyCode] = KbCheck;
+        if keyDown
+            if find(keyCode)>=min(vectKeyVal) && find(keyCode)<=max(vectKeyVal)
+                trialData.response = str2num(KbName(keyCode));
+                trialData.rt = secs - trialData.respScreenTime;
+                if trialData.response == trialData.dims
+                    trialData.correct = 1;
+                else
+                    trialData.correct = 0;
+                end
+            else
+                if keyCode(KbName('ESCAPE'))
+                    trialData.abortNow   = true;
+                end
+                trialData.validTrial = false;break;
+            end
+        end
+    end
+    FlushEvents('keyDown');
+end
+
+if trialData.response==999 % no response
+    trialData.validTrial = false;
+end
+
+drawFixation(expInfo, expInfo.fixationInfo);
+t = Screen('Flip', expInfo.curWindow);
+trialData.trialEndTime = t;
+
+trialData.stimDuration = trialData.stimEndTime - trialData.stimStartTime ; 
+trialData.trialDuration = trialData.trialEndTime - trialData.trialStartTime ; 
 
 end
 
