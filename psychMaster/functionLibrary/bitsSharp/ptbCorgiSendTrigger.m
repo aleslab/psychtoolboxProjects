fbf1aaf0a1e5984a0df43091600ed065c9e2d81e
Justin Ales <justin.ales@gmail.com>
2017-06-28 16:07:53 +0100

    First commit of working triggers.

--- psychMaster/functionLibrary/bitsSharp/ptbCorgiSendTrigger.m
+++ psychMaster/functionLibrary/bitsSharp/ptbCorgiSendTrigger.m
@@ -13,10 +13,12 @@ if nargin<2
     return;
 end
 
+highTime = 20; % time to be high in the beginning of the frame 
+lowTime = 248-highTime; % followed by x msec low (enough to fill the rest of the frame high + low = 24.8 ms)
 
-switch lower(varagin{1})
+switch lower(varargin{1})
     
-    case 'startRecording'
+    case 'startrecording'
         
         %SetupPulse
         triggerValue = expInfo.triggerInfo.startRecording;
@@ -25,14 +27,16 @@ switch lower(varagin{1})
         Screen('Flip', expInfo.curWindow);
         Screen('Flip', expInfo.curWindow);
 
-        disp('sending startRecording')
+        disp(['sending startRecording trigger value: ' num2str(triggerValue)]);
         
-    case 'conditionNumber'        
+    case 'conditionnumber'        
         triggerValue = varargin{2};
         pulseDef = [repmat(triggerValue,highTime,1);repmat(bin2dec('00000000000'),lowTime,1)]';
         BitsPlusPlus('DIOCommand', expInfo.curWindow, 1, 2^12-1, pulseDef, 0);
         Screen('Flip', expInfo.curWindow);
         Screen('Flip', expInfo.curWindow);
+        disp(['sending conditionNumber trigger value: ' num2str(triggerValue)]);
+
 end
 
 
