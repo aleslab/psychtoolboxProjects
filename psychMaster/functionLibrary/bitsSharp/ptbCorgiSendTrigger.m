f7b8ff5ba9b40166bc3f5ef2daaac5d2b1ca0882
Justin Ales <justin.ales@gmail.com>
2017-07-03 11:49:53 +0100

    Updating tigger to fix bug for time sent. and some defintion.

--- psychMaster/functionLibrary/bitsSharp/ptbCorgiSendTrigger.m
+++ psychMaster/functionLibrary/bitsSharp/ptbCorgiSendTrigger.m
@@ -1,28 +1,46 @@
 function [ timeSent ] = ptbCorgiSendTrigger( expInfo,command,sendNow,varargin )
-%UNTITLED2 Summary of this function goes here
-%function [ output_args ] = ptbCorgiSendTrigger( expInfo,trigger,sendNow,varargin )
+%ptbCorgiSendTrigger Handles sending various triggers
+%[ [timeSent] ] = ptbCorgiSendTrigger( expInfo,trigger,sendNow,varargin )
 %   Detailed explanation goes here
+%
+%
+%
+%Commands:
 
+timeSent = NaN; %If we haven't sent a trigger init as NaN;
 
 %If triggers aren't enables just silently return;
 if ~expInfo.enableTriggers
     return;
 end
 
+%If missing triggerInfo, rather than erroring out print a warning and keep
+%going. 
+if ~isfield(expInfo,'triggerInfo')
+   warning('ptbCorgi:sendTrigger:missingInfo','triggerInfo field missing, cannot send trigger!');
+   return;
+end
+
+%If wrong inputs error out.  
 if nargin<2
-    error('Error using ptbCorgiSendTrigger');
+    error('Error using ptbCorgiSendTrigger, see help ptbCorgiSendTrigger');
     return;
 end
 
 persistent toggleBitState; %Use a persistent variable to implement a toggle bit.
 
-highTime = 20; % time to be high in the beginning of the frame 
+
+%Bits sharp trigger times are in 100 MICROsecond chunks.  
+%I.e. 24.8 ms -> 248 100 microsecond. 
+%Length of data block is 248 so make sure we don't overflow with a min(). 
+highTime = min(248,round(expInfo.triggerInfo.pulseDuration*10)); % time to be high in the beginning of the frame 
 lowTime = 248-highTime; % followed by x msec low (enough to fill the rest of the frame high + low = 24.8 ms)
 
 maskNone = 2^11-1; %Use all bits. 
 maskToggle = bitand( maskNone,bitcmp(expInfo.triggerInfo.toggleBit,'uint16'));
 
 mask = maskToggle; %Default mask the toggle, that way other triggers don't set it. 
+yPos = 2;%Put triggers on the 2nd line. 
 switch lower(command)
     
     case {'clear','alllow','init','initialize'}
@@ -37,7 +55,11 @@ switch lower(command)
         disp(['sending startRecording trigger value: ' num2str(triggerValue)]);
         
     case 'conditionnumber' %Currently just sends the raw bits, but may change in future      
-        triggerValue = varargin{1};
+        triggerValue = varargin{1}+expInfo.triggerInfo.conditionNumberRange(1)-1; %Subtract one so the trigger range is inclusive
+        if triggerValue > expInfo.triggerInfo.conditionNumberRange(1)
+            warning('ptbCorgi:sendTrigger:condNumMaxExceeded','Sending condition number trigger value over range, still sending but check yourself');
+        end
+        
         disp(['sending conditionNumber trigger value: ' num2str(triggerValue)]);
 
     case 'raw'         
@@ -58,8 +80,8 @@ switch lower(command)
 end
 
 
-pulseDef = [repmat(triggerValue,highTime,1);repmat(0,lowTime,1)]';
-BitsPlusPlus('DIOCommand', expInfo.curWindow, 1, mask, pulseDef, 0);
+pulseDef = [repmat(triggerValue,highTime,1);zeros(lowTime,1)]';
+BitsPlusPlus('DIOCommand', expInfo.curWindow, 1, mask, pulseDef, 0,0, yPos);
 
 
 %if we want to send the trigger now we need to use 2 frames: 1 to send the
--- psychMaster/functionLibrary/bitsSharp/ptbCorgiTriggerDefault.m
+++ psychMaster/functionLibrary/bitsSharp/ptbCorgiTriggerDefault.m
@@ -9,11 +9,21 @@ if ~expInfo.useBitsSharp
     return;
 end
 
+%Setup pulse time;
+triggerInfo.pulseDuration  = 24.8; %Pulse duration in milliseconds, 
+%24.8 ms is max for bits#, If a duration longer than the ifi is specificed
+%the trigger stays in the state until the next trigger update is used. 
+
 %Define bit values for messages
 triggerInfo.startRecording = 2^8;
-triggerInfo.startTrial     = 1;
+triggerInfo.startTrial     = 64;
+triggerInfo.ssvepTagF1     = 1;
+triggerInfo.ssvepTagF2     = 2;
+triggerInfo.ssvepOddstep   = 4;
 triggerInfo.toggleBit      = 2^9;
-triggerInfo.messageBits    = 2^7-1; %Bits to enable for use as arbitrary messages
+triggerInfo.messageBitMask    = 2^8-1; %Bits to enable for use as arbitrary messages
+triggerInfo.conditionNumberRange = [ 101 165];
+
 
 %Allow user specified values to overwrite defaults.
 if isfield(expInfo,'triggerInfo') && ~isempty(expInfo.triggerInfo)
