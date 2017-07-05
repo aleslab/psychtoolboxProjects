cf651dd536c286c446eafe90f88da4157f66a540
Unknown <marlene.poncet@gmail.com>
2017-07-05 14:43:26 +0100

    Fixes for setup and trigger fixation

--- psychMaster/ptbCorgiSetup.m
+++ psychMaster/ptbCorgiSetup.m
@@ -3,7 +3,7 @@ function [] = setupPtbCorgi()
 luminanceCalibInfo=[];
 sizeCalibInfo     =[];
 
-
+setupPath();
 computerName = ptbCorgiGetComputerName();
 
 fh = figure('Visible','on','Units','normalized','Position',[.1 .2 .55 .6]);
@@ -484,6 +484,42 @@ updateCalibFileList();
         
     end
 
+
+         function setupPath()
+
+
+                %find where this function is being called from.
+                thisFile = mfilename('fullpath');
+                [thisDir, ~, ~] = fileparts(thisFile);
+
+                %For now just grab this and all subdirectories
+                newPath2Add = genpath(thisDir);
+
+                %Now let's find the directories that are missing and add only them
+                %to the path.
+                %Why not just add all sub directories to the path and let matlab
+                %auto prune redundancies? Well, that always brings the added
+                %directoreis top of the path. Which _may_ not be wanted from the
+                %user.
+                subDirCell = regexp(newPath2Add, pathsep, 'split');
+                pathCell = regexp(path, pathsep, 'split');
+
+                for iSub = 1:length(subDirCell)
+
+                    thisFolder = subDirCell{iSub};
+
+                    %If thisFolder doesn't match any of the directories on the path
+                    %add it to the path.
+                    if  ~any(strcmp(thisFolder, pathCell));
+                        msg = sprintf('Adding to path: %s',thisFolder);
+                        disp(msg);
+                        addpath(thisFolder);
+
+                    end
+
+                end
+
+            end
 
 end
 
