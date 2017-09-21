fe3a32299b1f8d73532ac2f1c7962e55ac4c5698
Marlene <marlene.poncet@gmail.com>
2017-09-21 12:08:09 +0100

    correction to currentTrial number (misplaced)

--- ptbCorgi/ptbCorgi.m
+++ ptbCorgi/ptbCorgi.m
@@ -413,18 +413,16 @@ disp('Use ptbCorgiSetup() to redefine defaults');
         
         
         iTrial = 1;
-        
-        %Adding some info about the current trial to expInfo. This is so
-        %trialFun functions can use it.
-        expInfo.currentTrial.number = iTrial;
-        
-        
+  
         expInfo = drawFixation(expInfo, expInfo.fixationInfo);
         Screen('Flip', expInfo.curWindow);
         
         while iTrial <=length(conditionList)
-     
-    
+            
+            %Adding some info about the current trial to expInfo. This is so
+            %trialFun functions can use it.
+            expInfo.currentTrial.number = iTrial;
+        
             validTrialList(iTrial)= true;  %initialize this index variable to keep track of bad/aborted trials
             experimentData(iTrial).validTrial = true;
             feedbackMsg = [];
