c95c4051cdaff4a245706398abda6db706044451
abigailrilee <abigailrilee@googlemail.com>
2018-01-08 00:43:43 +0000

    Removing psychMaster duplicate folder

--- psychMaster/paradigmFiles/Motion/Other/moveLineDefaultSettings.m
+++
@@ -1,7 +0,0 @@
-function [expInfo] = moveLineDefaultSettings(expInfo)
-
-expInfo.stereoMode = 4; %0 is monocular, 4 is split screen, 8 is anaglyph
-expInfo.viewingDistance = 97; %viewing distance including distance between mirrors and a cm to the eyes/chin rest is 97cm
-%expInfo.useFullScreen = true;
-expInfo.instructions = 'Which one \nchanged speed?\nPress any key\nto begin';
-expInfo.pauseInfo = 'Paused\nPress any keys\nto continue';
