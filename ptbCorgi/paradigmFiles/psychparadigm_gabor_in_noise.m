a7e024a4ba06347a12377a5b57b4071296d99b98
fraser <fa28@st-andrews.ac.uk>
2018-05-10 11:28:55 +0100

    tidy up paradigm

--- ptbCorgi/paradigmFiles/psychparadigm_gabor_in_noise.m
+++ ptbCorgi/paradigmFiles/psychparadigm_gabor_in_noise.m
@@ -43,34 +43,28 @@ conditionInfo(1).type='simpleresponse';
 % %Condition definitions
 %Condition 1, lets set some defaults:
 conditionInfo(1).stimDuration     = 0.5; %approximate stimulus duration in seconds
-conditionInfo(1).preStimDuration  = 1;  %Static time before stimulus change
-
+conditionInfo(1).post  = 1;  %Static time before stimulus change
+%conditionInfo(1).postStimMaskDuration =  .5; %mask duration in seconds. 
 conditionInfo(1).postStimDuration = 0;  %static time aftter stimulus change
 conditionInfo(1).iti              = 1;     %Minimum Inter Trial Interval
 conditionInfo(1).responseDuration  = 3;    %Post trial window for waiting for a response
-
-conditionInfo(1).sigma             =2; %standard deviation of the gabor in degrees
-conditionInfo(1).freq              =1; %frequency of the gabor in cycles per sigma. 
-conditionInfo(1).nReps             = 5; %% number of trials to present this condition. 
-conditionInfo(1).stimRadiusDeg     =   8;    %stimulus size in degree;
-
-conditionInfo(1).contrast = 0.50;
+conditionInfo(1).gaborOrientation = 90;
+conditionInfo(1).gaborSigma             =2; %standard deviation of the gabor in degrees
+conditionInfo(1).gaborFreq              =1; %frequency of the gabor in cycles per sigma. 
+conditionInfo(1).stimSizeDeg     =   8;    %stimulus size in degree;
+conditionInfo(1).gaborContrast = 0.50;
 conditionInfo(1).noiseSigma = .15;
-conditionInfo(1).orientationSigma = 0; %standard dev of the stim orientation change  
-%Implement arbitrary forward models. 
-%conditionInfo(1).forwardModel = [ 1 0 ]; %Forward model
-conditionInfo(1).label = 'Contrast: 0.50';
-conditionInfo(1).gaborCenterY      =   0;
-conditionInfo(1).gaborCenterX      =   0;
+
+conditionInfo(1).label = 'center';
+conditionInfo(1).gaborCenterY      =   0;%Horizontal location of gabor in degrees
+conditionInfo(1).gaborCenterX      =   0;%vertical location
 
 conditionInfo(2) = conditionInfo(1);
-conditionInfo(2).gaborContrast = 0.50 ;
 conditionInfo(2).label = 'Contrast: 0.50';
 conditionInfo(2).gaborCenterY      =   5;%just rough screen posisitions for now. 
 conditionInfo(2).gaborCenterX      =   6.5;%will see where to put in lab
 
 conditionInfo(3) = conditionInfo(1);
-conditionInfo(3).contrast = 0.50 ;
 conditionInfo(3).label = 'Contrast: 0.50';
 conditionInfo(3).gaborCenterY      =   2;
 conditionInfo(3).gaborCenterX      =   2;
