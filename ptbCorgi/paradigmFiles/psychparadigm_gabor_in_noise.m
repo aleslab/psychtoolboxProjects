6327eb403dacce2d3c7d30b9eeaf1bd9d14bcc44
fraser <fa28@st-andrews.ac.uk>
2018-06-04 17:33:46 +0100

    development of markov exp

--- ptbCorgi/paradigmFiles/psychparadigm_gabor_in_noise.m
+++ ptbCorgi/paradigmFiles/psychparadigm_gabor_in_noise.m
@@ -2,34 +2,45 @@ function [conditionInfo,expInfo] = psychparadigm_gabor_in_noise(expInfo)
 expInfo.paradigmName = 'gabor_in_noise';
 
 
-expInfo.trialRandomization.type = 'markov';
-expInfo.trialRandomization.nTrials = 20;
+expInfo.trialRandomization.type = 'blockedmarkov';
+expInfo.trialRandomization.nTrials = 100;
 
-expInfo.trialRandomization.transitionMatrix=[.3  0 .2 .5  0  0  0  0;  ...%if in con 1 .2 chance of staying in one
-                                             .5  0  0 .5  0  0  0  0; ...
-                                             .2 .3 .5  0  0  0  0  0;
-                                             .8  0  0 .2  0  0  0  0;
-                                              0  0  0  0 .8  0  0 .2;
-                                              0  0  0  0  0 .5 .3 .2;
-                                              0  0  0  0 .5  0  0 .5;
-                                              0  0  0  0 .5 .2  0 .3];
- 
+expInfo.trialRandomization.transitionMatrix=[1/9 6/9 1/9 1/9
+                                             1/9 1/9 6/9 1/9
+                                             6/9 1/9 1/9 1/9
+                                             1/3 1/3 1/3 0];
 
+expInfo.trialRandomization.type = 'blockedMarkov';
+   markovInfo(1).conditionList = [1 2 3 4];
+   markovInfo(1).transitionMatrix = [1/9 6/9 1/9 1/9
+                                     1/9 1/9 6/9 1/9
+                                     6/9 1/9 1/9 1/9
+                                     1/3 1/3 1/3 0];
+   markovInfo(1).nTrials = 80;
+   markovInfo(2).conditionList = [5 6 7 8];
+   markovInfo(2).transitionMatrix = [1/9 1/9 6/9 1/9
+                                     6/9 1/9 1/9 1/9
+                                     1/9 6/9 1/9 1/9
+                                     1/3 1/3 1/3 0];
+   markovInfo(2).nTrials = 20;
+   expInfo.trialRandomization.markovInfo = markovInfo;
+   expInfo.trialRandomization.groupOrder = [1 2 1 2 1 2]; % Say
+  
 
 
 % use kbQueue's as they have high performance
-expInfo.useKbQueue = false;
+expInfo.useKbQueue = true;
 expInfo.enablePowermate = false;
 expInfo.viewingDistance = 57;
 
-%fixationDuringStimulus=[expInfo.fixationInfo]; %
-% fixationInfo(1).type  = 'cross';
-% fixationInfo(1).size  = .5;
-% fixationInfo(1).lineWidthPix = 2;
-% fixationInfo(1).color = 1;
-% expInfo.fixationInfo = fixationInfo;
 
+fixationInfo(1).type  = 'cross';
+fixationInfo(1).size  = .5;
+fixationInfo(1).lineWidthPix = 2;
+fixationInfo(1).color = 1;
+expInfo.fixationInfo = fixationInfo;
 
+expInfo.conditionGroupingField = 'markovDir';
 
 %Lets add an experiment wide setting here:
 
@@ -41,7 +52,7 @@ expInfo.instructions = ['Where is the stimulus? its 10 points for every correct 
 %"function handle"
 conditionInfo(1).trialFun=@trial_gaborInNoise;
 conditionInfo(1).type='simpleresponse';
-
+conditionInfo(1).markovDir = 1;
 
 
 % %Condition definitions
@@ -49,33 +60,52 @@ conditionInfo(1).type='simpleresponse';
 conditionInfo(1).preStimDurationMin = 0.2;% minimum time before next stimulus
 conditionInfo(1).preStimDurationMu = 0.5; % prestimduration is drawn from a mu expotential distribution which has a "memeoryless" property
 conditionInfo(1).stimDuration     = 0.2; %approximate stimulus duration in seconds
-% conditionInfo(1). postStimMaskDuration=0; % no real need for a mask here 
-conditionInfo(1).gaborCenterX = 0; % going to be around 3 degs but il set this in the lab
-conditionInfo(1).gaborCenterY = 0; % see above 
+conditionInfo(1).postStimMaskDuration=0; % no real need for a mask here 
+conditionInfo(1).gaborCenterX = -3.5355; % going to be around 3 degs but il set this in the lab
+conditionInfo(1).gaborCenterY = 3.5355; % see above 
 conditionInfo(1).gaborPhase=90; % phase of gabor (i dont actually know what this means)
 conditionInfo(1).gaborOrientation = 0; % orientation of Gabor-doest really matter here as long as it is constant 
-conditionInfo(1).gaborContrast= .50; % picked 0.5 but anything over 0.20 is ok. 
+conditionInfo(1).gaborContrast= .25; % picked 0.5 but anything over 0.20 is ok. 
 conditionInfo(1).gaborSigma = 1; % standard dev of gaussian envlope
-conditionInfo(1).gaborFreq = 1; % sine wave freq in cycles per degree 
-conditionInfo(1).stimSizeDeg = 2; % size of stimulu in degrees
-conditionInfo(1).nosie = 0.15; % standard dev of the luminance values for noise mask
+conditionInfo(1).gaborFreq = 0.1; % sine wave freq in cycles per degree 
+conditionInfo(1).stimSizeDeg = 12; % size of stimulu in degrees
+conditionInfo(1).nosieSigma = 0.001; % standard dev of the luminance values for noise mask
 conditionInfo(1).label = 'state 1'; % label of position in markov
 
 conditionInfo(2) = conditionInfo(1);
 conditionInfo(2).label = 'state 2';
 conditionInfo(2).gaborCenterX = 0; % going to be around 3 degs but il set this in the lab
-conditionInfo(2).gaborCenterY = 0; % see above
+conditionInfo(2).gaborCenterY = 5; % see above
 
 conditionInfo(3) = conditionInfo(1);
 conditionInfo(3).label = 'state 3';
-conditionInfo(3).gaborCenterY      =   0;
-conditionInfo(3).gaborCenterX      =   0;
+conditionInfo(3).gaborCenterX     =  3.5355;
+conditionInfo(3).gaborCenterY     =  3.5355 ;
 
 conditionInfo(4) = conditionInfo(1);
-conditionInfo(4).GaborContrast = 0 ;
-conditionInfo(3).label = 'state 4 blank';
+conditionInfo(4).gaborContrast = 0 ;
+conditionInfo(4).label = 'blank';
 conditionInfo(4).gaborCenterY      =   0;
 conditionInfo(4).gaborCenterX      =   0;
+
+
+
+conditionInfo(5)=conditionInfo(1);
+conditionInfo(5).label = 'rev state 1';
+conditionInfo(5).markovDir = 2;
+
+conditionInfo(6)=conditionInfo(2);
+conditionInfo(6).label = 'rev state 2';
+conditionInfo(6).markovDir = 2;
+
+conditionInfo(7)=conditionInfo(3);
+conditionInfo(7).label = 'rev state 3';
+conditionInfo(7).markovDir = 2;
+
+conditionInfo(8)=conditionInfo(4);
+conditionInfo(8).label = 'rev blank';
+conditionInfo(8).markovDir = 2;
+
 
 
 end
