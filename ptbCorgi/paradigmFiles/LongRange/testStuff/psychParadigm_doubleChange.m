a33950dbf2fe3699961ba16c9bbbcc26029aaffd
Unknown <marlene.poncet@gmail.com>
2019-01-17 18:22:56 +0000

    check stimuli

--- ptbCorgi/paradigmFiles/LongRange/testStuff/psychParadigm_doubleChange.m
+++ ptbCorgi/paradigmFiles/LongRange/testStuff/psychParadigm_doubleChange.m
@@ -1,20 +1,21 @@
-function [conditionInfo, expInfo] = psychParadigm_dutyCycle_newStim(expInfo)
+function [conditionInfo, expInfo] = psychParadigm_doubleChange(expInfo)
+% instead of disapearing (to background color) the stimulus becomes white
+% is motion still present? 
 
 KbName('UnifyKeyNames');
 
 %paradigmName is what will be prepended to data files
-expInfo.paradigmName = 'dutyCycle_newStim';
+expInfo.paradigmName = 'DC_rating';
 expInfo.trialRandomization.type = 'blocked';
 expInfo.trialRandomization.blockByField = 'xloc'; % or just have a field group for each condition to determine which condition is in which group. Here xloc is the same for all conditions so all conditions are in each block
-expInfo.trialRandomization.nBlockReps   = 11; 
-% 10 repetitions * 14 s per trial (including resp) * 22 conditions = 50 min
+expInfo.trialRandomization.nBlockReps   = 8; 
 
 expInfo.viewingDistance = 57;
- 
-expInfo.useBitsSharp = true;
-expInfo.enableTriggers = true;
-% expInfo.useBitsSharp = false; 
-% expInfo.enableTriggers = false;
+
+% expInfo.useBitsSharp = true;
+% expInfo.enableTriggers = true;
+expInfo.useBitsSharp = false; 
+expInfo.enableTriggers = false;
 
 %Setup a simple fixation cross. See help drawFixation for more info on how
 %to setup this field.
@@ -23,7 +24,7 @@ expInfo.fixationInfo(1).size  = .2;
 expInfo.fixationInfo(1).lineWidthPix = 2;
 expInfo.fixationInfo(1).color = 0;
 
-expInfo.instructions = 'FIXATE the cross and count the number of dots appearing on the bar';
+expInfo.instructions = 'FIXATE the dot';
 
 %% General conditions
 conditionInfo(1).iti = 0.5; % inter-trial-interval
@@ -33,54 +34,55 @@ conditionInfo(1).giveFeedback = 0;
 conditionInfo(1).giveAudioFeedback = 0;
 conditionInfo(1).intervalBeep = 0;
 conditionInfo(1).maxToAnswer = 999; % next trial starts only after giving an answer % max time to answer
-conditionInfo(1).maxDots = 3; % max number of luminance change in a trial
 % conditionInfo(1).randomizeField = 'false';
 
 %% stimulus
 conditionInfo(1).stimSize = [0 0 0.5 20]; % in deg
-conditionInfo(1).xloc = 0; % eccentricity of stim centre from screen centre in deg
-conditionInfo(1).yloc = 5; % y eccentricity of stim centre
+conditionInfo(1).xloc = 3; % eccentricity of stim centre from screen centre in deg
+conditionInfo(1).yloc = 0; % y eccentricity of stim centre
+conditionInfo(1).trialFun=@trial_doubleChange;
 conditionInfo(1).trialDuration = 4*6*32/85; % in sec - around 9.0353 (or 100*8/85 or 50*16/85)
-conditionInfo(1).trialFun=@trial_dutyCycle_newStim;
 conditionInfo(1).motion = 0; % by default, no motion 
-conditionInfo(1).dotSize = [0 0 0.2 0.2];
-conditionInfo(1).preStimDuration = 3*32/85; % around 1.1294 s (or 6*16/85, 12*8/85)
 conditionInfo(1).xMotion = 0.6; % eccentricity from the other stim in motion condition (xStim = xloc + xMotion)
 conditionInfo(1).loc1 = 2; % xlocation of the 2nd stimulus IN ADDITION to the first stimulus
 conditionInfo(1).loc2 = 4; % x coord of the 3rd stimulus IN ADDITION to the first stimulus
 conditionInfo(1).horizBar = [0 0 conditionInfo(1).loc2+0.5 0.1];
+
 
 % same parameters in all conditions
 for cc=2:22
     conditionInfo(cc) = conditionInfo(1);
 end
 
+
 %% experimental manipulation
+
+condNb = 1;
+    
 % single stim condition
 testedFreq = [85/8 85/16 85/32]; % in Hz this is the onset of the single stimulus
 onTime = [1 2 4 6 7];
-condNb = 1;
+
+% for mot=0:1
+% for testFq=1:length(testedFreq)
+%     for tt = 1:length(onTime)
+%         conditionInfo(condNb).motion = mot;
+%         conditionInfo(condNb).stimTagFreq = testedFreq(testFq); 
+%         conditionInfo(condNb).dutyCycle = onTime(tt)/8;
+%         conditionInfo(condNb).label = [num2str(mot) 'mot' num2str(testedFreq(testFq),'%.1f') 'Hz ' num2str(onTime(tt)/8*100,'%.1f') '%'];
+%         condNb = condNb+1;
+%     end
+% end
+% end
+
 for testFq=1:length(testedFreq)
     for tt = 1:length(onTime)
         conditionInfo(condNb).stimTagFreq = testedFreq(testFq); 
         conditionInfo(condNb).dutyCycle = onTime(tt)/8;
-        conditionInfo(condNb).label = [num2str(testedFreq(testFq),'%.1f') 'Hz ' num2str(onTime(tt)/8*100,'%.1f') '% DC'];
+        conditionInfo(condNb).label = [num2str(testedFreq(testFq),'%.1f') 'Hz ' num2str(onTime(tt)/8*100,'%.1f') '%'];
         condNb = condNb+1;
     end
 end
-
-% % motion condition
-% testedFreq = [85/8 85/16]; % in Hz this is twice 1 cycle 
-% onTime = [2 6];
-% for tf = 1:length(testedFreq)
-%     for tt = 1:length(onTime)
-%         conditionInfo(condNb).stimTagFreq = testedFreq(tf);
-%         conditionInfo(condNb).dutyCycle = onTime(tt)/8;
-%         conditionInfo(condNb).motion = 1;
-%         conditionInfo(condNb).label = ['motion twice ' num2str(testedFreq(tf),'%.1f') 'Hz ' num2str(onTime(tt)/8*100,'%.1f') '% DC'];
-%         condNb = condNb+1;
-%     end
-% end
 
 testedFreq = [85/32 85/16 85/8 85/16 85/32]; % in Hz this is the onset of the single stimulus
 onTime = [1 2 4 6 7];
@@ -97,35 +99,11 @@ conditionInfo(condNb).stimTagFreq = 85/32;
 conditionInfo(condNb).dutyCycle = 4/8;
 conditionInfo(condNb).motion = 1;
 conditionInfo(condNb).label = ['motion ' num2str(85/32,'%.1f') 'Hz 50% DC'];
-
-conditionInfo(condNb+1).stimTagFreq = 85/16;
-conditionInfo(condNb+1).dutyCycle = 4/8;
-conditionInfo(condNb+1).motion = 1;
-conditionInfo(condNb+1).label = ['motion ' num2str(85/16,'%.1f') 'Hz 50% DC'];
-
-% % taking half cycle
-% testedFreq = [85/32 85/16 85/8]; % in Hz this is half cycle (onset of 1 stim)
-% onTime = 2;
-% for tt = 1:length(testedFreq)
-%     conditionInfo(condNb).stimTagFreq = testedFreq(tt);
-%     conditionInfo(condNb).dutyCycle = onTime/8;
-%     conditionInfo(condNb).motion = 1;
-%     conditionInfo(condNb).label = ['motion halfCycle ' num2str(testedFreq(tt)) 'Hz '];
-%     condNb = condNb+1;
-% end
-% % doubling the cycle
-% testedFreq = [85/16 85/8]; % in Hz this is twice 1 cycle 
-% onTime = [6 7];
-% for tf = 1:length(testedFreq)
-%     for tt = 1:length(onTime)
-%         conditionInfo(condNb).stimTagFreq = testedFreq(tf);
-%         conditionInfo(condNb).dutyCycle = onTime(tt)/8;
-%         conditionInfo(condNb).motion = 1;
-%         conditionInfo(condNb).label = ['motion twiceCycle ' num2str(testedFreq(tf)) 'Hz '];
-%         condNb = condNb+1;
-%     end
-% end
-
+condNb = condNb+1;
+conditionInfo(condNb).stimTagFreq = 85/16;
+conditionInfo(condNb).dutyCycle = 4/8;
+conditionInfo(condNb).motion = 1;
+conditionInfo(condNb).label = ['motion ' num2str(85/16,'%.1f') 'Hz 50% DC'];
 
 
 end
---
+++ ptbCorgi/trialFiles/LongRange/testStuff/trial_doubleChange.m
@@ -0,0 +1,197 @@
+function [trialData] = trial_doubleChange(expInfo, conditionInfo)
+% if the escape key is pressed then the experiment is aborted
+% press space to pause the experiment
+
+stimStartTime = 0;
+black = BlackIndex(expInfo.curWindow);
+white = WhiteIndex(expInfo.curWindow);
+
+drawFixation(expInfo, expInfo.fixationInfo);
+Screen('Flip', expInfo.curWindow);
+WaitSecs(0.1);
+drawFixation(expInfo, expInfo.fixationInfo);
+t = Screen('Flip', expInfo.curWindow);
+trialData.validTrial = true;
+trialData.abortNow   = false;
+trialData.trialStartTime = t;
+trialData.response = 999;
+ifi = expInfo.ifi;
+
+if expInfo.useBitsSharp
+    checkTiming = 1;
+else
+    checkTiming = 0; % timing of the nb of frames only checked for the "real" experiment using bitsharp
+end
+
+%%% VEP parameters
+framesPerCycle = 1/conditionInfo.stimTagFreq * round(expInfo.monRefresh);
+cycleDuration = 1/conditionInfo.stimTagFreq;
+monitorPeriodSecs = 1/round(expInfo.monRefresh);
+
+% framesPerCycle = cycleDuration / monitorPeriodSecs;
+framesOn = conditionInfo.dutyCycle * framesPerCycle;
+framesOff = framesPerCycle - framesOn;
+
+timeStimOn = monitorPeriodSecs * framesOn;
+timeStimOff = monitorPeriodSecs * framesOff;
+
+nbTotalCycles = ceil(conditionInfo.trialDuration * conditionInfo.stimTagFreq);
+trialDuration = nbTotalCycles * cycleDuration; % =11.67060200
+
+% save it in the data output structure
+trialData.framesPerCycle = framesPerCycle;
+trialData.framesOn = framesOn;
+trialData.framesOff = framesOff;
+trialData.timeStimOn = timeStimOn;
+trialData.timeStimOff = timeStimOff;
+trialData.nbTotalCycles = nbTotalCycles;
+trialData.trialDuration = trialDuration;
+trialData.cycleDuration = cycleDuration;
+
+
+
+%%% stim presentation
+rectStim = conditionInfo.stimSize*expInfo.ppd;
+ycoord = expInfo.center(2) - (conditionInfo.yloc * expInfo.ppd); % - above
+xcoord = expInfo.center(1) + (conditionInfo.xloc * expInfo.ppd); % + right
+eccMotion = xcoord + (conditionInfo.xMotion * expInfo.ppd); 
+loc1 = conditionInfo.loc1 * expInfo.ppd;
+loc2 = conditionInfo.loc2 * expInfo.ppd;
+
+% horizontal bar
+horizBar = conditionInfo.horizBar*expInfo.ppd;
+yBarTop = ycoord - conditionInfo.stimSize(4)/2*expInfo.ppd;
+yBarBottom = ycoord + conditionInfo.stimSize(4)/2*expInfo.ppd;
+
+
+% start trial
+for cycleNb = 1 : nbTotalCycles
+    % check if key is pressed in case needs to quit
+    [keyIsDown, secs, keyCode]=KbCheck(expInfo.deviceIndex);
+    if keyIsDown
+        trialData.validTrial = false;
+        if keyCode(KbName('escape'))
+            trialData.abortNow   = true;
+        elseif keyCode(KbName('space'))
+            trialData.validTrial = false;
+            Screen('DrawText', expInfo.curWindow, 'Taking a break', 0, expInfo.center(2), [0 0 0]);
+            Screen('DrawText', expInfo.curWindow, 'Press c to continue', 0, expInfo.center(2)+expInfo.center(2)/4, [0 0 0]);
+            Screen('Flip',expInfo.curWindow);
+            pressSpace = 1;
+            while pressSpace
+                [keyIsDown, secs, keyCode]=KbCheck(expInfo.deviceIndex);
+                if keyCode(KbName('c'))
+                    pressSpace = 0;
+                end
+            end
+        end
+        break;
+    end
+
+    %%% stim ON
+    drawFixation(expInfo, expInfo.fixationInfo);
+    if conditionInfo.motion == 1 && mod(cycleNb,2)==0 % in motion
+        Screen('FillRect', expInfo.curWindow, black,CenterRectOnPoint(rectStim,eccMotion,ycoord));
+        Screen('FillRect', expInfo.curWindow, black,CenterRectOnPoint(rectStim,eccMotion+loc1,ycoord));
+        Screen('FillRect', expInfo.curWindow, black,CenterRectOnPoint(rectStim,eccMotion+loc2,ycoord));
+        Screen('FillRect', expInfo.curWindow, black,CenterRectOnPoint(horizBar,eccMotion+loc1,yBarTop));
+        Screen('FillRect', expInfo.curWindow, black,CenterRectOnPoint(horizBar,eccMotion+loc1,yBarBottom));
+    else
+        Screen('FillRect', expInfo.curWindow, black,CenterRectOnPoint(rectStim,xcoord,ycoord));
+        Screen('FillRect', expInfo.curWindow, black,CenterRectOnPoint(rectStim,xcoord+loc1,ycoord));
+        Screen('FillRect', expInfo.curWindow, black,CenterRectOnPoint(rectStim,xcoord+loc2,ycoord));
+        Screen('FillRect', expInfo.curWindow, black,CenterRectOnPoint(horizBar,xcoord+loc1,yBarTop));
+        Screen('FillRect', expInfo.curWindow, black,CenterRectOnPoint(horizBar,xcoord+loc1,yBarBottom));
+    end
+    prevStim = t;
+    t = Screen('Flip', expInfo.curWindow, t + framesOff * ifi - ifi/2);
+    if cycleNb == 1
+        stimStartTime = t;
+    end
+    
+    %%% stim OFF
+    %Screen('FillRect', expInfo.curWindow, expInfo.bckgnd);
+    drawFixation(expInfo, expInfo.fixationInfo);
+    if conditionInfo.motion == 1 && mod(cycleNb,2)==0 % in motion
+        Screen('FillRect', expInfo.curWindow, white,CenterRectOnPoint(rectStim,eccMotion,ycoord));
+        Screen('FillRect', expInfo.curWindow, white,CenterRectOnPoint(rectStim,eccMotion+loc1,ycoord));
+        Screen('FillRect', expInfo.curWindow, white,CenterRectOnPoint(rectStim,eccMotion+loc2,ycoord));
+        Screen('FillRect', expInfo.curWindow, white,CenterRectOnPoint(horizBar,eccMotion+loc1,yBarTop));
+        Screen('FillRect', expInfo.curWindow, white,CenterRectOnPoint(horizBar,eccMotion+loc1,yBarBottom));
+    else
+        Screen('FillRect', expInfo.curWindow, white,CenterRectOnPoint(rectStim,xcoord,ycoord));
+        Screen('FillRect', expInfo.curWindow, white,CenterRectOnPoint(rectStim,xcoord+loc1,ycoord));
+        Screen('FillRect', expInfo.curWindow, white,CenterRectOnPoint(rectStim,xcoord+loc2,ycoord));
+        Screen('FillRect', expInfo.curWindow, white,CenterRectOnPoint(horizBar,xcoord+loc1,yBarTop));
+        Screen('FillRect', expInfo.curWindow, white,CenterRectOnPoint(horizBar,xcoord+loc1,yBarBottom));
+    end
+    t = Screen('Flip', expInfo.curWindow, t + framesOn * ifi - ifi/2 );
+        
+    if checkTiming
+        if t-prevStim > cycleDuration + ifi/2 || t-prevStim < cycleDuration - ifi/2
+            trialData.validTrial = false;
+            break;
+        end
+    end
+        
+end
+
+% this is to send a last trigger
+drawFixation(expInfo, expInfo.fixationInfo);
+prevStim = t;
+t = Screen('Flip', expInfo.curWindow, t + framesPerCycle * ifi - ifi/2);
+trialData.stimEndTime = t;
+% t-prevStim
+if checkTiming
+    if t-prevStim > cycleDuration + ifi/2 || t-prevStim < cycleDuration - ifi/2
+    trialData.validTrial = false;
+    end
+end
+
+trialData.stimStartTime = stimStartTime;
+
+% Find the key values (not the same in PC and MAC) for the response loop
+for keyVal=0:3
+    vectKeyVal(keyVal+1) = KbName(num2str(keyVal));
+end
+
+if trialData.validTrial
+    % response screen
+    Screen('DrawText', expInfo.curWindow, 'Did the stimulus move horizontally?', 0, expInfo.center(2)-expInfo.center(2)/2, [0 0 0]);
+    Screen('DrawText', expInfo.curWindow, '0. definitely not ', 0, expInfo.center(2), [0 0 0]);
+    Screen('DrawText', expInfo.curWindow, '1. probably not', 0, expInfo.center(2)+expInfo.center(2)/8, [0 0 0]);
+    Screen('DrawText', expInfo.curWindow, '2. probably yes', 0, expInfo.center(2)+expInfo.center(2)*2/8, [0 0 0]);
+    Screen('DrawText', expInfo.curWindow, '3. definitely yes', 0, expInfo.center(2)+expInfo.center(2)*3/8, [0 0 0]);
+    trialData.respScreenTime =Screen('Flip',expInfo.curWindow);
+    % check for key press
+    while trialData.response==999 % && (GetSecs < trialData.respScreenTime + conditionInfo.maxToAnswer -ifi/2)
+        [keyDown, secs, keyCode] = KbCheck;
+        if keyDown
+            if find(keyCode)>=min(vectKeyVal) && find(keyCode)<=max(vectKeyVal)
+                trialData.response = str2num(KbName(keyCode));
+                trialData.rt = secs - trialData.respScreenTime;
+            else
+                if keyCode(KbName('ESCAPE'))
+                    trialData.abortNow   = true;
+                end
+                trialData.validTrial = false;break;
+            end
+        end
+    end
+    FlushEvents('keyDown');
+end
+
+if trialData.response==999 % no response
+    trialData.validTrial = false;
+end
+
+drawFixation(expInfo, expInfo.fixationInfo);
+t = Screen('Flip', expInfo.curWindow);
+trialData.trialEndTime = t;
+
+trialData.trialDurationReal = trialData.stimEndTime - trialData.stimStartTime ;
+trialData.trialDurationTotal = trialData.trialEndTime - trialData.trialStartTime ;
+
+
+end
+
