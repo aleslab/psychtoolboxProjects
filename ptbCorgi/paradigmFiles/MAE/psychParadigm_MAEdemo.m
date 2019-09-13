7d71a39d5aaf8d81b0885f55b8a58efd308d7b75
Marlene <marlene.poncet@gmail.com>
2019-09-13 13:16:52 +0100

    demo on LCD monitor in EEG lab

--- ptbCorgi/paradigmFiles/MAE/psychParadigm_MAEdemo.m
+++ ptbCorgi/paradigmFiles/MAE/psychParadigm_MAEdemo.m
@@ -15,13 +15,21 @@ expInfo.trialRandomization.nBlockReps   = 4;
 expInfo.useBitsSharp = false; 
 expInfo.enableTriggers = false;
 
+% for LCD monitor
+expInfo.screenNum = 0;
+expInfo.requestedResolution.width = 1920;
+expInfo.requestedResolution.height = 1080;
+expInfo.requestedResolution.hz = 60;
+expInfo.requestedResolution.pixelSize = 24;
+expInfo.skipCalib = 1;
+    
 expInfo.fixationInfo(2).type  = 'cross';
 expInfo.fixationInfo(2).size  = .4;
 expInfo.fixationInfo(2).lineWidthPix = 3;
 expInfo.fixationInfo(2).color = 0;
 expInfo.fixationInfo(2).loc = [0 -5]; % location of the fixation relative to centre in degrees (1st number is horizontal, 2nd is vertical)
 expInfo.fixationInfo(1).type = 'noiseFrame';
-expInfo.fixationInfo(1).size = 5;
+expInfo.fixationInfo(1).size = 2;
 
 expInfo.instructions = 'FIXATE the cross';
 
@@ -39,9 +47,9 @@ conditionInfo(1).yEccentricity = 3;
 conditionInfo(1).f1 = 0.5; % in cycle (changes to c/deg in the trial pg)
 conditionInfo(1).tempFq = 85/18; % 4.72 Hz 
 conditionInfo(1).testFreq = 85/20; % 4.25 Hz
-conditionInfo(1).testDuration = 10; % in cycles. 5 seconds = 20/85*20 cycles (exactly 4.7 seconds)
-conditionInfo(1).adaptDuration = 5; % in sec: 10s top-up
-conditionInfo(1).longAdapt = 5; % 20 sec added to top-up for the 1st trial
+conditionInfo(1).testDuration = 20; % in cycles. 5 seconds = 20/85*20 cycles (exactly 4.7 seconds)
+conditionInfo(1).adaptDuration = 10; % in sec: 10s top-up
+conditionInfo(1).longAdapt = 20; % 20 sec added to top-up for the 1st trial
 
 %%%%%%%%%%%% parameters for the different conditions
 % spatial freq of the 2 gratings
@@ -68,7 +76,7 @@ spatialFq = [0.125 2]; % standard fq 0.5 /4 or *4
 phase = [5 90]; % cannot use a 180 (counterphase) because there is no clear energy motion direction for the SSVEP. 
 % with 180 there is no 1st harmonic which is the one showing a direction
 % selective response
-adaptDirection = [0 180 99]; % of standard 0=left, 180=right, 99=none (no drift)
+adaptDirection = [0 180]; % of standard 0=left, 180=right, 99=none (no drift)
 
 conditionTemplate = conditionInfo(1); %Take the first condition as the template
 conditionInfo = createConditionsFromParamList(conditionTemplate,'crossed',...
