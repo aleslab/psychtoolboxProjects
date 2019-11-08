6ee4baa90f60f2f0fd61e87204bfa25fc6c7fecd
Unknown <marlene.poncet@gmail.com>
2019-11-08 14:38:17 +0000

    v3 pb with responses

---
+++ ptbCorgi/paradigmFiles/MAE/psychParadigm_MAE_v3.m
@@ -0,0 +1,81 @@
+function [conditionInfo, expInfo] = psychParadigm_MAE_v3(expInfo)
+% overlapping gratings of 1 and 0.25 cycle/deg
+% test at 10 or 180 deg phase
+% adapt at 4.72Hz test at 4.25Hz
+% 30s adapt followed by 8 s test (1st s not used)
+% eeg processing: 5 epochs of 1.4 + 1 s = 8 s test: 1/85*20*6*5+(1/85*20*4)
+% triggers: 101 102 = not meaningful
+% only consider 111 to 132
+% ask the sbj to report the direction of the motion all the time although I
+% don't record it
+
+
+KbName('UnifyKeyNames');
+
+conditionInfo(1).direction = 'left';
+% choose from none, left, or right adaptation
+% sequence: none - L/R - none - L/R - none
+% odd sbj = left then right
+% even sbj = right then left
+
+
+if strcmp(conditionInfo(1).direction, 'none')
+    expInfo.trialRandomization.nBlockReps = 4;
+    condition = 10;
+else
+    expInfo.trialRandomization.nBlockReps = 12; 
+    if strcmp(conditionInfo(1).direction, 'left')
+        condition = 20;
+    elseif strcmp(conditionInfo(1).direction, 'right')
+        condition = 30;
+    end
+end
+
+
+%paradigmName is what will be prepended to data files
+expInfo.paradigmName = 'MAEv3';
+expInfo.viewingDistance = 57;
+expInfo.trialRandomization.type = 'custom';
+expInfo.giveAudioFeedback = 0;
+list = repmat(1:2,expInfo.trialRandomization.nBlockReps,1);
+expInfo.trialRandomization.trialList  = list(:)';
+expInfo.useKbQueue = true;
+
+% expInfo.useBitsSharp = true;
+% expInfo.enableTriggers = true;
+
+expInfo.fixationInfo(1).type  = 'dot';
+expInfo.fixationInfo(1).size  = .15; % radius of the dot
+expInfo.fixationInfo(1).loc = [0 0]; % location of the fixation relative to centre in degrees (1st number is horizontal, 2nd is vertical)
+
+expInfo.instructions = 'FIXATE the dot';
+expInfo.showTrialNb = 1; % give trial nb at the end of each trial (+ wait for keyboard)
+
+conditionInfo(1).iti = 0;
+conditionInfo(1).type = 'Generic';
+conditionInfo(1).giveFeedback = 0;
+conditionInfo(1).intervalBeep = 0;
+conditionInfo(1).trialFun=@trial_MAEv3;
+conditionInfo(1).stimSize = 24; % 24 grating image in degrees. 
+% should be an integer of 8 so that the lowest spatial frequency grating will
+% have full cycles only = the average luminance of the grating is equal to the background luminance 
+conditionInfo(1).tempFq = 85/18; % 85/18 or 85/16? 4.72 Hz 
+conditionInfo(1).testDuration = (20*6*5 + 20*4)/85; % in s (20*6*5 + 20*4)/85 = 8 s
+conditionInfo(1).adaptDuration = 1; % in sec: 30
+conditionInfo(1).f1 = 1; % cycle/deg
+conditionInfo(1).f2 = 0.25; % cycle/deg
+conditionInfo(1).testFreq = 85/20;
+
+%%%%%%%%%%%% parameters for the different conditions
+conditionTemplate = conditionInfo(1); 
+conditionInfo = createConditionsFromParamList(conditionTemplate,'pairwise',...
+    'phase',[10 180],...
+    'trigger',[1+condition 2+condition]); 
+
+end
+
+
+
+
+
+
---
+++ ptbCorgi/trialFiles/MAE/trial_MAEv3.m
@@ -0,0 +1,283 @@
+function [trialData] = trial_MAEv3(expInfo, conditionInfo)
+
+trialData.validTrial = true;
+trialData.abortNow   = false;
+trialData.response = 'none';
+abortExpTrigger = 99;
+
+
+% Find the color values which correspond to white and black.
+white=WhiteIndex(expInfo.curWindow);
+black=BlackIndex(expInfo.curWindow);
+gray=(white+black)/2;
+inc=white-gray;
+
+% Enable alpha blending
+Screen('BlendFunction', expInfo.curWindow, GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+
+% Create a special texture drawing shader for masked texture drawing:
+glsl = MakeTextureDrawShader(expInfo.curWindow, 'SeparateAlphaChannel');
+
+%%% timing test:
+nbTestCycles = conditionInfo.testDuration / (1/conditionInfo.testFreq);
+trialData.nbTestCycles = nbTestCycles;
+
+%%%%%%%%%%%%%%%%%
+%%%% Gratings
+texsize = conditionInfo.stimSize*expInfo.ppd; % Size of the grating image.
+cyclespersecond = conditionInfo.tempFq; % temporal frequency (related to velocity)
+
+% spatial freq of the 2 gratings
+f1 = conditionInfo.f1/expInfo.ppd; % cycle/deg
+f2 = conditionInfo.f2/expInfo.ppd; % cycle/deg
+
+% direction of the 2 gratings
+if strcmp(conditionInfo.direction,'left')
+    angle1 = 0;
+    angle2 = 180;
+elseif strcmp(conditionInfo.direction,'right')
+    angle1 = 180;
+    angle2 = 0;
+end
+trialData.dir1 = angle1;
+trialData.dir2 = angle2;
+
+% Calculate parameters of the grating:
+fr1=f1*2*pi;
+fr2=f2*2*pi;
+
+% Create gratings:
+% the 2 gratings are created with the same sin 0 such that it gives a more
+% 'edgy' stimulus (which is more natural and that the brain likes)
+x = meshgrid(-texsize:texsize, -texsize:texsize);
+grating1 = gray + inc*sin(fr1*x);
+grating2 = gray + inc*sin(fr2*x);
+
+% add alpha column
+grating1 = repmat(grating1,[1,1,3]);
+grating1(:,:,4) = ones(size(grating1,1));
+grating2 = repmat(grating2,[1,1,3]);
+grating2(:,:,4) = ones(size(grating2,1))*.5;
+
+% Store alpha-masked grating in texture and attach the special 'glsl'
+% texture shader to it:
+gratingAdapt1 = Screen('MakeTexture', expInfo.curWindow, grating1 , [], [], [], [], glsl);
+gratingAdapt2 = Screen('MakeTexture', expInfo.curWindow, grating2 , [], [], [], [], glsl);
+   
+%%% shift
+switch conditionInfo.f1
+    case 0.25
+        counterphaseF1 = 2;
+    case 0.5
+        counterphaseF1 = 1;
+    case 1
+        counterphaseF1 = 0.5;
+    case 2
+        counterphaseF1 = 0.25;
+end
+switch conditionInfo.f2
+    case 0.25
+        counterphaseF2 = 2;
+    case 0.5
+        counterphaseF2 = 1;
+    case 1
+        counterphaseF2 = 0.5;
+    case 2
+        counterphaseF2 = 0.25;
+end
+
+phaseDiv = 180/conditionInfo.phase;
+testShiftF1 = counterphaseF1 / phaseDiv * expInfo.ppd;
+testShiftF2 = counterphaseF2 / phaseDiv * expInfo.ppd;
+
+
+% Definition of the drawn source rectangle on the screen:
+srcRect=[0 0 texsize texsize];
+
+%%%%%%%%%%%%%%%%%
+%%% timing for presentation
+waitframes = 1;
+waitduration = waitframes * expInfo.ifi; 
+
+% Recompute p, this time without the ceil() operation from above.
+% Otherwise we will get wrong drift speed due to rounding!
+p1 = 1/f1; % pixels/cycle
+p2 = 1/f2; % pixels/cycle
+
+% Translate requested speed of the gratings (in cycles per second) into
+% a shift value in "pixels per frame", assuming given waitduration:
+shiftperframe1 = cyclespersecond * p1 * waitduration;
+shiftperframe2 = cyclespersecond * p2 * waitduration;
+
+% parameters for test
+cycleDuration = 1/conditionInfo.testFreq;
+framesPerCycle = 1/conditionInfo.testFreq * round(expInfo.monRefresh);
+framesPerHalfCycle = framesPerCycle/2;
+
+% Perform initial Flip to sync us to the VBL and for getting an initial
+% VBL-Timestamp for our "WaitBlanking" emulation:
+if expInfo.useBitsSharp
+    trialData.trigger = conditionInfo.trigger;
+    ptbCorgiSendTrigger(expInfo,'conditionnumber',true,conditionInfo.trigger);
+end
+    
+if expInfo.useBitsSharp
+    f1Trigger = expInfo.triggerInfo.ssvepTagF1;
+    checkTime = 1;
+    ptbCorgiSendTrigger(expInfo,'starttrial',true);
+else
+    f1Trigger = 1;
+    checkTime = 0;
+end
+
+drawFixation(expInfo, expInfo.fixationInfo);
+vbl = Screen('Flip', expInfo.curWindow);
+trialData.trialStartTime = vbl;
+
+vblAdaptTime = vbl + conditionInfo.adaptDuration;
+trialData.adaptTime = vblAdaptTime;
+
+% testShift = 0.25 * expInfo.ppd;
+% Screen('DrawTexture', expInfo.curWindow, gratingAdapt1, srcRect, CenterRectOnPoint(srcRect,expInfo.center(1),expInfo.center(2)+yEcc),[], [], [], [], [], [], [0, 0, 0, 0]);
+% Screen('DrawTexture', expInfo.curWindow, gratingAdapt1, srcRect, CenterRectOnPoint(srcRect,expInfo.center(1),expInfo.center(2)-yEcc),[], [], [], [], [], [], [0, testShift, 0, 0]);
+% Screen('Flip', expInfo.curWindow);
+
+
+keysOfInterest=zeros(1,256);
+keysOfInterest(KbName({'1' '2' '3' 'ESCAPE'}))=1;
+KbQueueCreate(expInfo.deviceIndex, keysOfInterest);
+KbQueueStart(expInfo.deviceIndex);
+KbQueueFlush();
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ADAPTATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+i = 0; % incremental shift of the gratings 
+if strcmp(conditionInfo.direction,'none')
+    %%%%%%%%%%%%%%%%% No adaptation
+    while (vbl < vblAdaptTime) && ~trialData.abortNow 
+        i=i+1;
+        Screen('DrawTexture', expInfo.curWindow, gratingAdapt1, srcRect, CenterRectOnPoint(srcRect,expInfo.center(1),expInfo.center(2)));
+        Screen('DrawTexture', expInfo.curWindow, gratingAdapt2, srcRect, CenterRectOnPoint(srcRect,expInfo.center(1),expInfo.center(2)));
+        drawFixation(expInfo, expInfo.fixationInfo); 
+        vbl = Screen('Flip', expInfo.curWindow, vbl + (waitframes - 0.5) * expInfo.ifi);
+        [pressed, firstPress]=KbQueueCheck(expInfo.deviceIndex);
+        if firstPress(KbName('ESCAPE'))
+            trialData.abortNow =1;
+        end
+    end
+else
+    %%%%%%%%%%%%%%%%%
+    %%% Adaptation loop: Run for 10 s or keypress.
+    while (vbl < vblAdaptTime) && ~trialData.abortNow 
+        
+        % Shift the grating by "shiftp2perframe" pixels per frame. We pass
+        % the pixel offset 'yoffset' as a parameter to
+        % Screen('DrawTexture'). The attached 'glsl' texture draw shader
+        % will apply this 'yoffset' pixel shift to the RGB or Luminance
+        % color channels of the texture during drawing, thereby shifting
+        % the gratings. Before drawing the shifted grating, it will mask it
+        % with the "unshifted" alpha mask values inside the Alpha channel:
+        yoffset1 = mod(i*shiftperframe1,p1);
+        yoffset2 = mod(i*shiftperframe2,p2);
+        i=i+1;
+        
+        % Draw gratings texture, rotated by "angle":
+        Screen('DrawTexture', expInfo.curWindow, gratingAdapt1, srcRect, CenterRectOnPoint(srcRect,expInfo.center(1),expInfo.center(2)), angle1, [], [], [], [], [], [0, yoffset1, 0, 0]);
+        Screen('DrawTexture', expInfo.curWindow, gratingAdapt2, srcRect, CenterRectOnPoint(srcRect,expInfo.center(1),expInfo.center(2)), angle2, [], [], [], [], [], [0, yoffset2, 0, 0]);
+        drawFixation(expInfo, expInfo.fixationInfo);       
+        % Flip 'waitframes' monitor refresh intervals after last redraw.
+        vbl = Screen('Flip', expInfo.curWindow, vbl + (waitframes - 0.5) * expInfo.ifi);
+        [pressed, firstPress]=KbQueueCheck(expInfo.deviceIndex);
+        if firstPress(KbName('ESCAPE'))
+            trialData.abortNow =1;
+        end
+
+    end
+    
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TEST %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% test presented right after the end of adaptation = no stimulus onset that
+% would create an ERP so that we can record SSVEP earlier 
+
+cycle = 0;
+trialData(:).response=zeros(1,nbTestCycles);
+while cycle<nbTestCycles && trialData.validTrial 
+    [pressed, firstPress]=KbQueueCheck(expInfo.deviceIndex);
+    if firstPress(KbName('ESCAPE'))
+        trialData.abortNow =1;
+    end
+        
+    % first stim
+    Screen('DrawTexture', expInfo.curWindow, gratingAdapt1, srcRect, CenterRectOnPoint(srcRect,expInfo.center(1),expInfo.center(2)),[], [], [], [], [], [], [0, 0, 0, 0]);
+    Screen('DrawTexture', expInfo.curWindow, gratingAdapt2, srcRect, CenterRectOnPoint(srcRect,expInfo.center(1),expInfo.center(2)),[], [], [], [], [], [], [0, 0, 0, 0]);
+    drawFixation(expInfo, expInfo.fixationInfo);
+    ptbCorgiSendTrigger(expInfo,'raw',0,f1Trigger);
+    vbl = Screen('Flip', expInfo.curWindow, vbl + (framesPerHalfCycle - 0.5) * expInfo.ifi);
+    vbl1 = vbl;
+    if cycle == 0
+        trialData.trialTestTime = vbl;
+    elseif checkTime
+        if vbl1 - vbl2 > cycleDuration/2 + expInfo.ifi/2 || vbl1 - vbl2 < cycleDuration/2 - expInfo.ifi/2
+            trialData.validTrial = false;
+        end
+    end
+
+    % second stim
+    Screen('DrawTexture', expInfo.curWindow, gratingAdapt1, srcRect, CenterRectOnPoint(srcRect,expInfo.center(1),expInfo.center(2)),[], [], [], [], [], [], [0, testShiftF1, 0, 0]);
+    Screen('DrawTexture', expInfo.curWindow, gratingAdapt2, srcRect, CenterRectOnPoint(srcRect,expInfo.center(1),expInfo.center(2)),[], [], [], [], [], [], [0, testShiftF2, 0, 0]);
+    drawFixation(expInfo, expInfo.fixationInfo);
+    ptbCorgiSendTrigger(expInfo,'clear',0);
+    vbl = Screen('Flip', expInfo.curWindow, vbl + (framesPerHalfCycle - 0.5) * expInfo.ifi);
+    vbl2 = vbl;
+    if checkTime
+        if vbl2 - vbl1 > cycleDuration/2 + expInfo.ifi/2 || vbl2 - vbl1 < cycleDuration/2 - expInfo.ifi/2
+            trialData.validTrial = false;
+        end
+    end
+    
+    % increment cycle
+    cycle = cycle+1;
+end
+
+drawFixation(expInfo, expInfo.fixationInfo);
+if expInfo.useBitsSharp
+    if trialData.validTrial
+        ptbCorgiSendTrigger(expInfo,'raw',0,f1Trigger);
+    else
+        ptbCorgiSendTrigger(expInfo,'raw',1,abortExpTrigger); % abort trial
+    end
+end
+trialData.trialEndTime = Screen('Flip', expInfo.curWindow, vbl + (framesPerHalfCycle - 0.5) * expInfo.ifi);
+if trialData.validTrial
+    trialData.testDuration = trialData.trialEndTime - trialData.trialTestTime;
+    trialData.trialDuration = trialData.trialEndTime - trialData.trialStartTime;
+end
+
+while KbEventAvail(expInfo.deviceIndex)
+    [trialData.evt] = KbEventGet(expInfo.deviceIndex);
+end 
+KbQueueRelease(expInfo.deviceIndex);
+KbEventFlush(expInfo.deviceIndex);
+
+%%%% close all the textures
+Screen('Close', [gratingAdapt1, gratingAdapt2]);
+
+ptbCorgiSendTrigger(expInfo,'clear',0);
+drawFixation(expInfo, expInfo.fixationInfo);
+Screen('Flip', expInfo.curWindow);
+
+
+if expInfo.useBitsSharp
+    ptbCorgiSendTrigger(expInfo,'endtrial',true);
+end
+    
+end
+
+
